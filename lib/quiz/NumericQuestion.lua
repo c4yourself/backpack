5ad9d20eeb1f7f46f1fb8a4e87174967851cd3c2
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-10-04 11:08:05 +0200

    Refactored the NumericQuestion.lua file

--- lib/quiz/NumericQuestion.lua
+++ lib/quiz/NumericQuestion.lua
@@ -7,24 +7,18 @@ local NumericQuestion = class("NumericQuestion")
 
 --- Constructor for NumericQuestion
 -- @param image_path path to the image as a string
--- @param question string
+-- @param question string representing a mathematical expression
 -- @param correct_answer number representing the correct answer
 function NumericQuestion:__init(image_path, question, correct_answer)
 	self.correct_answer = correct_answer
 	self.question = question
 end
 
---- Creates a random mathematical question
--- @param level difficulty of the question generated
---function NumericQuestion:_create_question(level)
---	print("TODO: Implement random generator")
---	return "1+1"
---end
-
 -- Evaluates if a mathematical expression is correct
 -- @param user_answer Number representing the user answer
+-- @return Boolean value
 function NumericQuestion:_is_correct (user_answer)
-	return user_answer == load("return " .. self.question)()
+	return user_answer == self.correct_answer
 end
 
 return NumericQuestion
