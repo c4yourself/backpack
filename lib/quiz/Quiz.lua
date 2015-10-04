274d0ab502715bd282c56416613f20ab4c5ff005
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-10-04 14:54:11 +0200

    Added a first version of Quiz.lua

---
+++ lib/quiz/Quiz.lua
@@ -0,0 +1,33 @@
+--- Base class for Quiz
+-- @classmod Quiz
+-- @field questions table with Strings representing questions
+-- @field current_question counter keeping track of the current question
+-- @field correct_answers counter keeping track of the correct answers
+-- @field wrong_answers keeping track of the wrong answers
+
+local class = require("lib.classy")
+
+local Quiz = class("Quiz")
+
+--- Constructor for the Quiz class
+function Quiz:__init()
+	self.questions = {}
+	self.current_question = 0
+	self.correct_answers = 0
+	self.wrong_answers = 0
+end
+
+--- Returns the next question in the quiz
+-- @return String representing the next question
+function Quiz:get_question()
+	self.current_question = self.current_question + 1
+	return self.questions[self.current_question]
+end
+
+--- Checks if the users answer to the current question is correct
+-- @return Boolean to show if the answer was correct or not
+function Quiz:answer(answer)
+	return self.questions[current_question]:is_correct(answer)
+end
+
+return Quiz
