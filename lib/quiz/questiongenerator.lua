f479549622785444179f3eff71801fc24cb7ba3b
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-10-04 10:49:58 +0200

    Refactored the questiongenerator module

--- lib/quiz/questiongenerator.lua
+++ lib/quiz/questiongenerator.lua
@@ -1,13 +1,11 @@
 --- Question generator module. Module used for generating random numeric questions
 -- @module questiongenerator
 
--- Require math library
---local questiongenerator = require(math)
-
 -- Module table
 local questiongenerator = {}
 
--- table mapping the constant difficulty levels to a number of operands
+-- table mapping the constant difficulty levels to the number of operands that
+-- will be included in the question
 questiongenerator.diff_operand_map = {
 	["BEGINNER"] = 2;
 	["NOVICE"] = 3;
@@ -15,6 +13,7 @@ questiongenerator.diff_operand_map = {
 	["EXPERT"] = 8
 }
 
+-- table mapping different difficulty levels to different operators
 questiongenerator.token_table = {
 	["BEGINNER"] = {"+";"-"},
 	["NOVICE"] = {"+";"-";"*"},
@@ -23,7 +22,11 @@ questiongenerator.token_table = {
 }
 
 --- Generates a numerical (arithmetic) question based on the difficulty level
+-- Input string should be one of the constants in the questiongenerator module,
+-- i.e. BEGINNER, NOVICE, ADVANCED, or EXPERT
 -- @param difficulty String representing the difficulty level
+-- @return question Mathematical expression as a String
+-- @return answer The correct answer to the question as a Number
 function questiongenerator.generate(level)
 	local number_of_operands = questiongenerator.diff_operand_map[level]
 	local tokens = questiongenerator.token_table[level]
