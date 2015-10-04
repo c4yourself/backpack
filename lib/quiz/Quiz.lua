354ec1744aef16f032226248f43c2c3f27c2aae5
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-10-04 16:05:16 +0200

    Minor refactoring in the Quiz.lua class

--- lib/quiz/Quiz.lua
+++ lib/quiz/Quiz.lua
@@ -8,7 +8,6 @@
 local class = require("lib.classy")
 local questiongenerator = require("lib.quiz.questiongenerator")
 local NumericQuestion = require("lib.quiz.NumericQuestion")
-
 local Quiz = class("Quiz")
 
 --- Constructor for the Quiz class
@@ -29,16 +28,12 @@ end
 --- Checks if the users answer to the current question is correct
 -- @return Boolean to show if the answer was correct or not
 function Quiz:answer(answer)
-	print("CQ " .. self.current_question)
-	print("Type")
-	print(type(self.questions[self.current_question]))
-	print(self.questions[self.current_question].correct_answer)
 	return self.questions[self.current_question]:is_correct(answer)
 end
 
--- Generates a numerical quiz
+-- Generates a numerical quiz of a given size and difficulty
 function Quiz:generate_numerical_quiz(level, quiz_size, image_path)
---TODO add image path
+	--TODO image_path needs to be properly implemented
 	for i=1, quiz_size do
 		local question, answer = questiongenerator.generate(level)
 		local question = NumericQuestion(image_path, question, answer)
