7fc40d31442ee4864c2c59fa05deeba0ccc00105
Daniel Axelsson (tester) <danax519@student.liu.se>
2015-10-06 16:33:00 +0200

    added scoreboard and changed colors on the menu

--- views/menu.lua
+++ views/menu.lua
@@ -9,11 +9,16 @@ function menu.render(surface)
 	surface:clear(backgroundColor)
 	surface:copyfrom(gfx.loadpng(utils.absolute_path("data/images/paris.png")))
 
-	local buttonColor = {r=0, g=0, b=0}
-	local textColor = {r=155, g=100, b=255}
-	local textButton1 = sys.new_freetype(textColor, 20, {x=200,y=80}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-	local textButton2 = sys.new_freetype(textColor, 20, {x=200,y=280}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-	local textButton3 = sys.new_freetype(textColor, 20, {x=200,y=480}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local buttonColor = {r=0, g=128, b=225}
+	local exit_buttonColor = {r=255, g=102, b=0}
+	local textColor = {r=0, g=0, b=0}
+	local score_textColor = {r=255, g=255, b=255}
+	local textButton1 = sys.new_freetype(textColor, 30, {x=200,y=80}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local textButton2 = sys.new_freetype(textColor, 30, {x=200,y=280}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local textButton3 = sys.new_freetype(textColor, 30, {x=200,y=480}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local score = sys.new_freetype(score_textColor, 40, {x=1010,y=170}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+
+	score:draw_over_surface(surface, "Score: " .. "125")
 
 	surface:fill(buttonColor, {width=500, height=100, x=100, y=50})
 	textButton1:draw_over_surface(surface, "1. Numerical quiz")
@@ -21,7 +26,7 @@ function menu.render(surface)
 	surface:fill(buttonColor, {width=500, height=100, x=100, y=250})
 	textButton2:draw_over_surface(surface, "2. Multiple choice question")
 
-	surface:fill(buttonColor, {width=500, height=100, x=100, y=450})
+	surface:fill(exit_buttonColor, {width=500, height=100, x=100, y=450})
 	textButton3:draw_over_surface(surface, "3. Exit")
 
 	event.remote_control:on("button_release", function(button)
