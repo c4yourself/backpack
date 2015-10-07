046c845deb01cc0c28bed7e0c71a65869d55a4da
Linbo He (Developer) <linhe979@student.liu.se>
2015-10-07 16:33:18 +0200

    modified Quetion.lua and multiple_quiz.lua

--- views/multiple_quiz.lua
+++ views/multiple_quiz.lua
@@ -1,6 +1,8 @@
 local event = require("lib.event")
 local utils = require("lib.utils")
 local quest = require("lib.quiz.Question")
+local MultiQuest = require("lib.quiz.MultipleChoiceQuestion")
+
 local multiple_quiz = {}
 
 
@@ -8,6 +10,8 @@ function multiple_quiz.render(surface)
    --event.remote_control:on("button_release" ,function(key)
    surface:fill({r=255,g=255,b=255})
 		-- surface:clear({r=80,g=90,b=100})
+		Question1=MultiQuest("path","How are you?",{1,2,3})
+	    Question1:show("DroidSans-Bold",{80,50,222},surface)
 		 gfx:update()
 
 
