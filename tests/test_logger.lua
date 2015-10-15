8f36cc7610287aca62eff8864d25eadef152831c
Fredrik Lofberg (testleader) <frelo206@student.liu.se>
2015-10-15 15:58:21 +0200

    trying jenkins build

--- tests/test_logger.lua
+++ tests/test_logger.lua
@@ -26,11 +26,6 @@ function TestLogger:test_defaults()
 	luaunit.assertFalse(self.logger.log_memory)
 end
 
--- Test to break jenkins
-function TestLogger:test_break()
-	luaunit.assertFalse(true)
-end
-
 
 -- Test that log level is respected
 function TestLogger:test_level()
