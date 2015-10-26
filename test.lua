da780a3297fa8af54f7d58c5cc9e004f3638d2e5
Alma Jonsson (Technical Writer) <almjo303@student.liu.se>
2015-10-26 14:38:00 +0100

    added test

---
+++ test.lua
@@ -0,0 +1,11 @@
+local connectfour = require("lib.connectfour")
+local a = connectfour.ConnectFour()
+
+print(a:serialize())
+
+--[[print(a:get(6,4))
+print(a:get(2,3))
+print(a:get(6,1))--]]
+
+--print(a:get_player())
+print(a:is_valid_move("O",1))
