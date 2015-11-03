1f04e664310e0c67d9e06fe7a735697841de6bbd
Daniel Axelsson (Tester) <danax519@student.liu.se>
2015-11-03 13:57:05 +0100

    created a clear function for surface mock

--- lib/mocks/SurfaceMock.lua
+++ lib/mocks/SurfaceMock.lua
@@ -1,18 +1,69 @@
-
 local class = require("lib.classy")
-local logger = require("lib.logger")
 local SurfaceMock = class("SurfaceMock")
 
+--Constructor for SurfaceMock.
 function SurfaceMock:__init(width, height)
+  self.width = width
+  self.height = height
+  local c = {
+		r = 0,
+		g = 0,
+		b = 0,
+		a = 0
+	}
   self.pixels = {}
-  for i= 1, heigth do
+  for i= 0, (width-1) do
     self.pixels[i] = {}
-    for j = 1, widht do
-      self.pixels[i][j] = 0
+    for j = 0, (height-1) do
+      self.pixels[i][j] = c
     end
   end
 end
 
+--Fills desired area odf the surface with a solid color.
 function SurfaceMock:clear(color, rectangle)
+  --Set the color to the required color, default is black.
+  local c = {
+		r = 0,
+		g = 0,
+		b = 0,
+		a = 0
+	}
+	if color ~= nil then
+		c.r = color.r or 0
+		c.g = color.g or 0
+		c.b = color.b or 0
+		c.a = color.a or 255
+	end
 
+  --locates the desireable rectangle, default is whole surface.
+  local rect = {
+    x = 0,
+    y = 0,
+    width = self.width,
+    height = self.height)
+  }
+
+  if rectangle ~= nil then
+    rect.x = (rectangle.x or 0)
+    rect.y = (rectangle.y or 0)
+
+    if rect.x + (rectangle.width or rectangle.w) <= rect.width then
+      rect.width = (rectangle.width or rectangle.w)
+    end
+
+    if rect.y + (rectangle.height or rectangle.h) <= rect.height then
+      rect.height = (rectangle.height or rectangle.h)
+    end
+  end
+
+  --Change color on the specified rectangle, default whole surface gets
+  --repainted.
+  local w = rect.x + rect.width - 1
+  local h = rect.y + rect.height - 1
+  for i = rect.x, w do
+    for j = rect.y, h do
+      self.pixels[i][j] = c
+    end
+  end
 end
