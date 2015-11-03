705bf1600e67bf57b17210cba76e6f559a87faa7
Fredrik Lofberg (testleader) <frelo206@student.liu.se>
2015-11-03 23:04:51 +0100

    fixed some indentation

--- tests/test_mock_SurfaceMock.lua
+++ tests/test_mock_SurfaceMock.lua
@@ -7,18 +7,17 @@ local TestMockSurface = {}
 
 --Setup width = 720, height = 1080
 function TestMockSurface:setUp()
-    self.SurfaceMock = SurfaceMock(720, 1080)
+	self.SurfaceMock = SurfaceMock(720, 1080)
 end
 
 --Tests the default dimension of the surface
 function TestMockSurface:test_default_dimensions()
-    --luaunit.assertEquals(self.SurfaceMock.get_width(), 720)
-    --luaunit.assertEquals(self.SurfaceMock.get_height(), 1080)
+	--luaunit.assertEquals(self.SurfaceMock.get_width(), 720)
+	--luaunit.assertEquals(self.SurfaceMock.get_height(), 1080)
 end
 
 --Tests that the default color of the surface is black
 function TestMockSurface:test_default_color()
-
 end
 
 
--- tests/test_numerical.lua
+++ tests/test_numerical.lua
@@ -7,11 +7,11 @@ local NumericQuestion = require("lib.quiz.NumericQuestion")
 local TestNumerical = {}
 
 function TestNumerical:setUp()
-  num_q = NumericQuestion("", "11+1", 12)
+	num_q = NumericQuestion("", "11+1", 12)
 end
 
 function TestNumerical:test_is_correct()
-  luaunit.assertEquals(num_q:is_correct(12), true)
+	luaunit.assertEquals(num_q:is_correct(12), true)
 end
 
 return TestNumerical
