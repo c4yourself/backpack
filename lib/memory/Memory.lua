b5c7883025ab5ee4c416afc62a3c3933db18e28b
Fredrik Urby <freur817@student.liu.se>
2015-11-06 11:48:40 +0100

    Added open method

--- lib/memory/Memory.lua
+++ lib/memory/Memory.lua
@@ -1,17 +1,77 @@
 
 local class = require("lib.classy")
-local Question = require("lib.memory.Memory")
-
+local Memory = class("Memory")
 
 --- Constructor for Memory
--- @param image_path path to the image as a string
--- @param question string representing a mathematical expression
--- @param correct_answer number representing the correct answer
+-- @param paris is an integer
+-- @param scrambled is false, then the pairs should be {1,1,2,2,etc.}
 function Memory:__init(pairs, scrambled)
-	if (scrambled != nil) {
-		self.scrambled = scrambled
-	} else {
+	if scrambled == false then
+		self.scrambled = false
+ 	else
 		self.scrambled = true
-	}
+	end
 	self.pairs = pairs
+	self.cards = _create_pairs()
+	self.moves = 0
+	self.state = {}
+
+	for i = 1, pairs do
+		table.insert(self.state, false)
+		table.insert(self.state, false)
+	end
+
+	-- if (scrambled ~= nil) {
+	-- 	self.scrambled = scrambled
+	-- } else {
+	-- 	self.scrambled = true
+	-- }
+	-- self.pairs = pairs
+end
+
+--- Generates pairs for the board
+-- @return a is table of pairs for the memory
+-- @local
+function Memory:_create_pairs()
+--Provide a better name for a?
+	local	a = {}
+	if self.scrambled == false then
+		for i = 1, self.pairs do
+			table.insert(a,i)
+			table.insert(a,i)
+		end
+	else
+		for i = 1, self.pairs do
+			local d = math.random(self.pairs)
+			table.insert(a,d)
+			table.insert(a,d)
+		end
+	end
+	return a
+end
+
+
+function Memory:look(index)
+
+end
+
+function Memory:open(index)
+	if index > table.getn(state) then
+		error("Index is greater than number of cards")
+	elseif state(index) == true then
+		error("Card is already open")
+	else
+		state[index] = true
+		self.moves = self.moves + 1
+		return cards[index]
+	end
+
+function Memory:is_finished()
+
+end
+
+function Memory:serialize(columns)
+end
+
+
 end
