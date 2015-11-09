32bc5c4c4e9c058589122d321ef3c90ee5492714
Fredrik Lofberg (testleader) <frelo206@student.liu.se>
2015-11-10 00:19:13 +0100

    Added some tests to test_mock_SurfaceMock

--- tests/test_mock_SurfaceMock.lua
+++ tests/test_mock_SurfaceMock.lua
@@ -10,15 +10,15 @@ function TestMockSurface:test_negative_setUp()
 end
 
 
---Setup width = 720, height = 1080
+--Setup width = 1080, height = 720
 function TestMockSurface:setUp()
-	self.surface_mock = SurfaceMock(720, 1080)
+	self.surface_mock = SurfaceMock(1080, 720)
 end
 
 --Tests the default dimension of the surface
 function TestMockSurface:test_default_dimensions()
-	luaunit.assertEquals(self.surface_mock:get_width(), 720)
-	luaunit.assertEquals(self.surface_mock:get_height(), 1080)
+	luaunit.assertEquals(self.surface_mock:get_width(), 1080)
+	luaunit.assertEquals(self.surface_mock:get_height(), 720)
 end
 
 --Tests that the default color of the surface is black
@@ -30,7 +30,7 @@ function TestMockSurface:test_default_color()
 	luaunit.assertEquals({self.surface_mock:get_pixel(0, self.surface_mock:get_height() - 1)}, {0, 0, 0, 0})
 end
 
-
+--Tests that the new colors are correct
 function TestMockSurface:test_clear()
 	rect = {
 		x = 0,
@@ -45,7 +45,32 @@ function TestMockSurface:test_clear()
 		a = 50
 	}
 	self.surface_mock:clear(c, rect)
+	luaunit.assertEquals({self.surface_mock:get_pixel(0, 0)}, {50, 50, 50, 50})
+	luaunit.assertEquals({self.surface_mock:get_pixel(500, 500)}, {0, 0, 0, 0})
+	luaunit.assertEquals({self.surface_mock:get_pixel(0, 499)}, {50, 50, 50, 50})
+	luaunit.assertEquals({self.surface_mock:get_pixel(1079, 719)}, {0, 0, 0, 0})
 end
+
+--inits a too big size and then tries to clear a smaller area within
+--Should it return an error?
+function TestMockSurface:test_init_too_big()
+	rect = {
+		x = 0,
+		y = 0,
+		width = 1300,
+		height = 1300
+	}
+	c = {
+		r = 100,
+		g = 100,
+		b = 100,
+		a = 100
+	}
+	self.surface_mock = SurfaceMock(1500, 1500)
+	self.surface_mock:clear(c, rect)
+	luaunit.assertEquals({self.surface_mock:get_pixel(1200, 1200)}, {100, 100, 100, 100})
+end
+
 
 
 
