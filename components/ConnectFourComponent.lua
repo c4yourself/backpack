d6de562fb3885b1fc02df5924c589076a3400179
Alma Jonsson (Technical Writer) <almjo303@student.liu.se>
2015-11-09 09:56:40 +0100

    Started creating the board for ConnectFour

--- components/ConnectFourComponent.lua
+++ components/ConnectFourComponent.lua
@@ -22,24 +22,48 @@ function ConnectFourComponent:render(surface)
 	width_coinbox = (1/7)*(0.45)*surface:get_width()
 	height_coinbox = (1/7)*(0.8)*surface:get_height()
 	--surface:clear({r=255, g=255, b=255}, {x=10, y=10, width = surface:get_width()*0.5, height = surface:get_height()*0.5})
-	surface:clear({r=255, g=255, b=255}, {x=10, y=10, width = width_coinbox, height = height_coinbox})
-	surface:clear({r=255, g=255, b=255}, {x=100, y=100, width = width_coinbox, height = height_coinbox})
+	surface:clear({r=255, g=255, b=255}, {x=10, y=surface:get_height()-height_coinbox, width = width_coinbox, height = height_coinbox})
+	--surface:clear({r=255, g=255, b=255}, {x=100, y=100, width = width_coinbox, height = height_coinbox})
 
+	local posy = 0.1*surface:get_height()
+	local tempcolor = 255
+
+	for i = 1, 7 do
+		local posx = 0.35*surface:get_width()
+        for j = 1, 7 do
+          surface:clear({r=tempcolor, g=tempcolor, b=tempcolor}, {x=posx, y=posy, width = width_coinbox, height = height_coinbox})
+					posx = posx + width_coinbox
+					tempcolor = tempcolor - 5
+        end
+
+				posy = posy + height_coinbox
+    end
 
 	local coin_color_player = {r=255, g=255, b=51}
 	local coin_color_computer = {r=255, g=0, b=0}
 
-	local text_color = {r=0, g=0, b=0}
-	local game_title = sys.new_freetype(textColor, 30, {x=100,y=400}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-	local choiceButton2 = sys.new_freetype(textColor, 30, {x=350,y=400}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+surface:fill({r=255, g=255, b=255}, {width=200, height=60, x=0.05*surface:get_width(),y=0.9*surface:get_height()-1.5*height_coinbox})
+	local choiceButton = sys.new_freetype({r = 255, g = 128, b = 0, a = 255}, 22,
+	{x=0.05*surface:get_width(),y=0.9*surface:get_height()-1.5*height_coinbox}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	choiceButton:draw_over_surface(surface,"Back to the city")
 
-	--box = sys.new_freetype(
-	--{r = 0, g = 0, b = 0, a = 255},
-	--coin_color_computer,
-	--72,
-	--{x = 300, y = 300},
-	--utils.absolute_path("data/fonts/DroidSans.ttf"))
-	--box:draw_over_surface(surface, surface:get_width() .. "x" .. surface:get_height())
+
+	local heading = sys.new_freetype({r = 255, g = 128, b = 0, a = 255}, 36,
+	{x = 0.05 * surface:get_width() , y = 0.1 * surface:get_height() },
+	utils.absolute_path("data/fonts/DroidSans.ttf"))
+	heading:draw_over_surface(surface, "Connect Four")
+
+	local text_player = sys.new_freetype(coin_color_player, 22,
+	{x = 0.05 * surface:get_width() , y = 0.5*surface:get_height()-2*height_coinbox },
+	utils.absolute_path("data/fonts/DroidSans.ttf"))
+	text_player:draw_over_surface(surface, "Player")
+
+	local text_computer = sys.new_freetype(coin_color_computer, 22, {x = 0.05 * surface:get_width() , y = 0.5*surface:get_height()+0.1*height_coinbox },
+	utils.absolute_path("data/fonts/DroidSans.ttf"))
+	text_computer:draw_over_surface(surface, "Computer")
+
+	surface:clear(coin_color_player, {x=0.1*surface:get_width(), y=0.5*surface:get_height()-1.5*height_coinbox, width = width_coinbox, height = height_coinbox})
+	surface:clear(coin_color_computer, {x=0.1*surface:get_width(), y=0.5*surface:get_height()+0.5*height_coinbox, width = width_coinbox, height = height_coinbox})
 
 end
 
