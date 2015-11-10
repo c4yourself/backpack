19d187019b17caa249321e03ded6c79d4f7903f0
Fredrik Vendelström (Deploymentmanager) <freve985@student.liu.se>
2015-11-10 13:57:20 +0100

    Top Row GUI

--- components/ConnectFourComponent.lua
+++ components/ConnectFourComponent.lua
@@ -5,20 +5,49 @@ local View = require("lib.view.View")
 local utils = require("lib.utils")
 
 local ConnectFourComponent = class("ConnectFourComponent", View)
+ current_column = 4
 
 function ConnectFourComponent:__init(remote_control)
 	View.__init(self)
 
-	if remote_control ~= nil then
-		self.event_listener = remote_control
-	else
-		self.event_listener = event.remote_control
-	end
+	self.connectfour = ConnectFour()
 
+	self:listen_to(
+	event.remote_control,
+	"button_release",
+	utils.partial(self.press, self)
+	)
 end
 
+function ConnectFourComponent:press(key)
+	if key == "right" then
+		current_column = current_column + 1
+		self.dirty(false)
+		self.dirty(true)
+	else if key == "left" then
+		current_column = current_column - 1
+		self.dirty(false)
+		self.dirty(true)
+	end
+end
+
+function ConnectFourComponent:top_row(column)
+	local posx = 0.35*surface:get_width()
+	local color = {r = 0, g = 0, b = 0}
+	for j = 1, 7 do
+		if j == column then
+			color = {r=255, g=255, b=51}
+		end
+		surface:clear(color, {x=posx, y=posy, width = width_coinbox, height = height_coinbox})
+		posx = posx + width_coinbox
+		color = {r = 0, g = 0, b = 0}
+	end
+end
 
 function ConnectFourComponent:render(surface)
+	local coin_color_player = {r=255, g=255, b=51}
+	local coin_color_computer = {r=255, g=0, b=0}
+
 	width_coinbox = (1/7)*(0.45)*surface:get_width()
 	height_coinbox = (1/7)*(0.8)*surface:get_height()
 	--surface:clear({r=255, g=255, b=255}, {x=10, y=10, width = surface:get_width()*0.5, height = surface:get_height()*0.5})
@@ -28,7 +57,9 @@ function ConnectFourComponent:render(surface)
 	local posy = 0.1*surface:get_height()
 	local tempcolor = 255
 
-	for i = 1, 7 do
+	ConnectFourComponent:top_row(current_column)
+
+	for i = 1, 6 do
 		local posx = 0.35*surface:get_width()
         for j = 1, 7 do
           surface:clear({r=tempcolor, g=tempcolor, b=tempcolor}, {x=posx, y=posy, width = width_coinbox, height = height_coinbox})
@@ -37,12 +68,11 @@ function ConnectFourComponent:render(surface)
         end
 
 				posy = posy + height_coinbox
-    end
+  end
 
-	local coin_color_player = {r=255, g=255, b=51}
-	local coin_color_computer = {r=255, g=0, b=0}
 
-surface:fill({r=255, g=255, b=255}, {width=200, height=60, x=0.05*surface:get_width(),y=0.9*surface:get_height()-1.5*height_coinbox})
+
+	surface:fill({r=255, g=255, b=255}, {width=200, height=60, x=0.05*surface:get_width(),y=0.9*surface:get_height()-1.5*height_coinbox})
 	local choiceButton = sys.new_freetype({r = 255, g = 128, b = 0, a = 255}, 22,
 	{x=0.05*surface:get_width(),y=0.9*surface:get_height()-1.5*height_coinbox}, utils.absolute_path("data/fonts/DroidSans.ttf"))
 	choiceButton:draw_over_surface(surface,"Back to the city")
