84f5441cb539cd5d959c5e3f154c75e813b8a0c1
Johan Wikstrom <jwwikstrom@gmail.com>
2015-11-11 11:28:03 +0100

    Allmost all buttons are done

--- views/CityView.lua
+++ views/CityView.lua
@@ -27,21 +27,24 @@ function CityView:__init(remote_control)
 end
 
 function CityView:render(surface)
+	-- Creates local variables for height and width
+	local height = surface:get_height()
+	local width = surface:get_width()
 	-- Resets the surface and draws the background
 	local background_color = {r=0, g=0, b=0}
 	surface:clear(background_color)
 	surface:copyfrom(gfx.loadpng(utils.absolute_path("data/images/paris.png")))
 
 	--creates some colors
-	local button_color = color(111, 244, 225, 255)
+	local button_color = color(255, 99, 0, 255)
+	local color_selected = color(255, 153, 0, 255)
+	local color_disabled = color(111, 222, 111, 255) --have not been used yet
 	local text_color = color(0, 0, 0,255)
 	local score_text_color = color(255, 255, 255, 255)
 	local menu_bar_color = color(0, 0, 0, 225)
 	local status_bar_color = color(0, 0, 0, 255)
 	local status_text_color = color(255, 255, 255, 255)
-	local color_selected = color(33, 99, 111, 255)
-	local color_disabled = color(111, 222, 111, 255) --have not been used yet
-
+	local experience_bar_color = color(100, 100, 100, 255)
 
 	-- Creates some images
 	-- local coinsurface = gfx.new_surface(50,50)
@@ -54,17 +57,21 @@ function CityView:render(surface)
 	local profile_name = sys.new_freetype(status_text_color, 25, {x=10, y=10}, utils.absolute_path("data/fonts/DroidSans.ttf"))
 	local profile_level = sys.new_freetype(status_text_color, 20, {x=200, y=15}, utils.absolute_path("data/fonts/DroidSans.ttf"))
 	local profile_experience = sys.new_freetype(status_text_color, 20, {x=440, y=15}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-	local profile_cash = sys.new_freetype(status_text_color, 20, {x=surface:get_width()-100, y=15}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-	local city_name = sys.new_freetype(status_text_color, 25, {x=surface:get_width()/2, y=10}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local profile_cash = sys.new_freetype(status_text_color, 20, {x=width-100, y=15}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local city_name = sys.new_freetype(status_text_color, 25, {x=width/2, y=10}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local mini_games = sys.new_freetype(status_text_color, 25, {x=width/6-65, y=60}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+	local options = sys.new_freetype(status_text_color, 25, {x=width/6-45, y=(height-50)/2+60}, utils.absolute_path("data/fonts/DroidSans.ttf"))
 
 	-- Shows menu bar
-	surface:fill(menu_bar_color, {width=surface:get_width()/3, height=surface:get_height(), x=0, y=0})
+	surface:fill(menu_bar_color, {width=width/3, height=height-50, x=0, y=50})
+	mini_games:draw_over_surface(surface, "Mini Games")
+	options:draw_over_surface(surface, "Options")
 
 	-- Implement status bar
-	surface:fill(status_bar_color, {width=surface:get_width(), height=50, x=0, y=0})
+	surface:fill(status_bar_color, {width=width, height=50, x=0, y=0})
 	surface:fill(score_text_color, {width=150, height=30, x=285,y=10})
 		if(self.profile.experience/500~=1) then
-		surface:fill(status_bar_color, {width=math.ceil(148*(1-self.profile.experience/500)), height=28, x=434-148*(1-self.profile.experience/500), y=11})
+		surface:fill(experience_bar_color, {width=math.ceil(148*(1-self.profile.experience/500)), height=28, x=434-148*(1-self.profile.experience/500), y=11})
 	end
 
 	profile_name:draw_over_surface(surface, self.profile.name)
@@ -77,27 +84,53 @@ function CityView:render(surface)
 	local button_1 = button(button_color, color_selected, color_disabled,true,true)
 	local button_2 = button(button_color, color_selected, color_disabled,true,false)
 	local button_3 = button(button_color, color_selected, color_disabled,true,false)
+	local button_4 = button(button_color, color_selected, color_disabled,true,false)
+	local button_5 = button(button_color, color_selected, color_disabled,true,false)
+	local button_6 = button(button_color, color_selected, color_disabled,true,false)
+	local button_7 = button(button_color, color_selected, color_disabled,true,false)
+	local button_8 = button(button_color, color_selected, color_disabled,true,false)
 
 	-- Define each button's posotion and size
-	local position_1={x=100,y=50}
-	local button_size_1 = {width=500,height=100}
+	local position_1={x=3*width/45,y=100}
+	local button_size_1 = {width=4*width/45,height=4*width/45}
 
-	local position_2={x=100,y=250}
-	local button_size_2 = {width=500,height=100}
+	local position_2={x=8*width/45,y=100}
+	local button_size_2 = {width=4*width/45,height=4*width/45}
 
-	local position_3={x=100,y=450}
-	local button_size_3 = {width=500,height=100}
+	local position_3={x=3*width/45,y=100+5*width/45}
+	local button_size_3 = {width=4*width/45,height=4*width/45}
+
+	local position_4={x=8*width/45,y=100+5*width/45}
+	local button_size_4 = {width=4*width/45,height=4*width/45}
+
+	local position_5={x=3*width/45,y=(height-50)/2+100}
+	local button_size_5 = {width=4*width/45,height=4*width/45}
+
+	local position_6={x=8*width/45,y=(height-50)/2+100}
+	local button_size_6 = {width=4*width/45,height=4*width/45}
+
+	local position_7={x=3*width/45,y=5*width/45+(height-50)/2+100}
+	local button_size_7 = {width=4*width/45,height=4*width/45}
+
+	local position_8={x=8*width/45,y=5*width/45+(height-50)/2+100}
+	local button_size_8 = {width=4*width/45,height=4*width/45}
 
 	-- Using the button grid to create buttons
 	self.buttonGrid:add_button(position_1,button_size_1,button_1)
 	self.buttonGrid:add_button(position_2,button_size_2,button_2)
 	self.buttonGrid:add_button(position_3,button_size_3,button_3)
+	self.buttonGrid:add_button(position_4,button_size_4,button_4)
+	self.buttonGrid:add_button(position_5,button_size_5,button_5)
+	self.buttonGrid:add_button(position_6,button_size_6,button_6)
+	self.buttonGrid:add_button(position_7,button_size_7,button_7)
+	self.buttonGrid:add_button(position_8,button_size_8,button_8)
+
 
   -- Insert text and its color, position, size, path for each button
   -- Button always has the screen as its background, not its subsurface
-	button_1:set_textdata("Numerical quiz",text_color,{x=100,y=50},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_2:set_textdata("Multiple choice question",text_color,{x=100,y=250},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_3:set_textdata("Exit",text_color,{x=100,y=450},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
+	--button_1:set_textdata("Numerical quiz",text_color,{x=100,y=50},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
+	--button_2:set_textdata("Multiple choice question",text_color,{x=100,y=250},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
+	--button_3:set_textdata("Exit",text_color,{x=100,y=450},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
 
  -- Adding the score to surface
 	local score = sys.new_freetype(score_text_color:to_table(), 40, {x=1010,y=170}, utils.absolute_path("data/fonts/DroidSans.ttf"))
@@ -105,10 +138,11 @@ function CityView:render(surface)
 
   -- using the button grid to render all buttons and texts
 	self.buttonGrid:render(surface)
+	surface:copyfrom(gfx.loadpng(utils.absolute_path("data/images/GeoIcon.png")),nil, {x=position_1.x,y=position_1.y,width=button_size_1.width,height=button_size_1.height} )
 
   -- testing the subsurface
-	local sub_surface1 = SubSurface(surface,{width=100, height=100, x=0, y=0})
-	sub_surface1:clear({r=255, g=255, b=255, a=255})
+	--local sub_surface1 = SubSurface(surface,{width=100, height=100, x=0, y=0})
+	--sub_surface1:clear({r=255, g=255, b=255, a=255})
 
 
 
