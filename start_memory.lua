0dc4e87df2a418ccd688d18e482ae015307941ce
Lisa Wedlund <lisa.wedlund@gmail.com>
2015-11-11 11:55:17 +0100

    added start_memory.lua

---
+++ start_memory.lua
@@ -0,0 +1,37 @@
+local event = require("lib.event")
+local logger = require("lib.logger")
+local utils = require("lib.utils")
+local menu = require("views.menu")
+
+local view = require("lib.view")
+local CityView = require("views.CityView")
+local memory_view = require("views.MemoryView")
+
+--- This function runs every time a key is pressed
+-- The current mapping for the emulator can be found in emulator/zto.lua
+-- @param key Key that was pressed
+-- @param state Either up or repeat
+function onKey(key, state)
+	logger.trace("Remote control input (" .. key .. ", " .. state .. ")")
+
+	--testing remote control
+	if state == "down" then
+		event.remote_control:trigger("button_press", key)
+	elseif state == "up" then
+		event.remote_control:trigger("button_release", key)
+	else
+		event.remote_control:trigger("button_repeat", key)
+	end
+
+	-- Terminate program when exit key is pressed
+	if key == "exit" and state == "up" then
+		sys.stop()
+	end
+end
+
+-- This function is called at the start of the program
+function onStart()
+	local memory_view = MemoryView()
+	view.view_manager:set_view(memory_view)
+	gfx.update()
+end
