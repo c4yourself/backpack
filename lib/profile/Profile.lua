220e9d5aa0c61a4cd8ce7542b6b3c7b3451c08b2
Huanyu Li <huali592@student.liu.se>
2015-11-11 01:11:46 +0100

    Basic implementation of profile class

---
+++ lib/profile/Profile.lua
@@ -0,0 +1,85 @@
+--- Profile
+-- @classmod Profile
+
+local class = require("lib.classy")
+local utils = require("lib.utils")
+local Profile = class("Profile")
+local event = require("lib.event")
+
+Profile.name = ""
+Profile.email_address = ""
+Profile.date_of_birth = ""
+Profile.sex = ""
+Profile.balance = 0
+Profile.experience = 0
+Profile.password = ""
+
+--- Constructor for Profile
+-- @param name string representing name of user
+-- @param email_address string representing email address of user
+-- @param date_of_birth string date birth of user
+-- @param sex string representing the gender of the user
+function Profile:__init(name,email_address,date_of_birth,sex)
+	self.name = name
+	self.email_address = email_address
+	self.date_of_birth = date_of_birth
+	self.sex = sex
+end
+
+-- Set balance of the user
+-- @param balance representing balance of the user
+function Profile:set_balance(balance)
+	self.balance = balance
+end
+
+-- Set experience of the user
+-- @param experience representing experience of the user
+function Profile:set_experience(experience)
+	self.experience = experience
+end
+
+-- Set password of the user
+-- @param password representing password of the user
+function Profile:set_password(password)
+	self.password = password
+end
+
+-- Modify balance of the user
+-- @param number representing the change of balance
+function Profile:modify_balance(number)
+	self.balance = self.balance + number
+	return self.balance
+end
+
+-- Modify experience of the user
+-- @param number representing the change of experience
+function Profile:modify_experience(number)
+	if number >= 0 then
+		self.experience = self.experience + number
+	end
+	return self.experience
+end
+
+-- Save profile
+function Profile:save()
+	local path = utils.absolute_path(string.format("data/profile/%s.profile",self.name))
+	local file = io.open(path,"w");
+	file:write("{\n")
+	file:write("\t\t\"badges\": ,\n")
+	file:write("\t\t\"balance\": " .. self.balance .. ",\n")
+	file:write("\t\t\"date_of_birth\": \"" .. self.date_of_birth .. "\",\n")
+	file:write("\t\t\"email_address\": " .. self.email_address .. "\",\n")
+	file:write("\t\t\"experience\": " .. self.experience .. ",\n")
+	file:write("\t\t\"id\": ,\n")
+	file:write("\t\t\"inventory\": {}\n")
+	file:write("\t\t\"login_token\": \"token\",\n")
+	file:write("\t\t\"name\": \"" .. self.name .. "\",\n")
+	file:write("\t\t\"password\": \"" .. self.password .. "\",\n")
+	file:write("\t\t\"sex\": \"" .. self.sex .. "\",\n")
+	file:write("}")
+end
+-- load profile
+function Profile:load(filename)
+end
+
+return Profile
