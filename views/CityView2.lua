fa8ed62915cd96242ed19b711ba461d9838bd61c
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-11-11 10:52:04 +0100

    Refactored MultipleChoiceView and CityView2. Removed unused code.

--- views/CityView2.lua
+++ views/CityView2.lua
@@ -67,7 +67,7 @@ function CityView2:render(surface)
 	button_2:set_textdata("Multiple choice question",text_color,{x=100,y=250},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
 	button_3:set_textdata("Exit",text_color,{x=100,y=450},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
 
- -- Adding the score to surface
+ 	-- Adding the score to surface
 	local score = sys.new_freetype(score_text_color:to_table(), 40, {x=1010,y=170}, utils.absolute_path("data/fonts/DroidSans.ttf"))
 	score:draw_over_surface(surface, "Score: " .. "125")
 
@@ -121,9 +121,6 @@ function CityView2:load_view(button)
 		-- TODO ^This should be done by a subsurface in the final version
 		gfx.update()
 	elseif button == "2" then
-		--multiplechoice_quiz.render(screen)
-		--gfx.update()
-
 		local mult_quiz_view = MultipleChoiceView()
 		--Stop listening to everything
 		self:stop_listening(event.remote_control)
--- views/MultipleChoiceView.lua
+++ views/MultipleChoiceView.lua
@@ -17,10 +17,10 @@ function MultipleChoiceView:__init()
 
 	-- Flags (and similiar)
 	self.listening_initiated = false
-	self.end_flag=0
-	self.check_question_flag=0
-	self.last_check=1
-	self.quiz_state = "IDLE"
+	self.end_flag = 0 -- To indicate if the quiz if complete
+	self.check_question_flag = 0
+	self.last_check = 1
+	self.quiz_state = "IDLE" -- To keep track of quiz state
 	--Components
 
 	-- Logic
@@ -54,25 +54,29 @@ end
 -- global @{ViewManager} instance). This method handles the logic and determines
 -- what should be diplayed next to the user
 function MultipleChoiceView:press(key)
-	-- Determine what should happen next
-	if key == "right" and self.last_check == self.current_question  then
-		for j=1,#self.user_input,1 do
-			self.answer[j]=tonumber(string.sub(self.user_input,j,j))
+	-- Determine what should happen depending on the user input and current state
+	if key == "right" and self.last_check == self.current_question
+		and self.quiz_state ~= "DONE" then
+		for j = 1, #self.user_input, 1 do
+			self.answer[j] = tonumber(string.sub(self.user_input,j,j))
 		end
-		if self.mult_choice_quiz.questions[self.current_question]:is_correct(self.answer)==true then
+		if self.mult_choice_quiz.questions[self.current_question]:is_correct(self.answer) == true then
 			self.correct_answer_number = self.correct_answer_number + 1
-			self.result_string = "Right and You answered " .. self.correct_answer_number .. " questions correctly."
+			self.result_string = "Right. You've answered "
+				.. self.correct_answer_number .. " questions correctly."
 			self.last_check = self.last_check + 1
 		else
-			self.result_string = "Wrong and You answered " .. self.correct_answer_number .. " questions correctly."
+			self.result_string = "Wrong. You've answered "
+				.. self.correct_answer_number .. " questions correctly."
 			self.last_check=self.last_check + 1
 		end
 		self.quiz_state = "DISPLAY_RESULT"
 		self:dirty(true)
-		self.answer={}
-		self.user_input=""
-	--display the next question
-	elseif key=="right" and self.last_check==self.current_question+1 and end_flag~=1 then
+		--Reset user input after the answer has been initiated
+		self.answer = {}
+		self.user_input = ""
+	elseif key == "right" and self.last_check == self.current_question + 1
+		and end_flag ~= 1 then
 		-- Next question is displayed
 		-- Make sure there are questions left to display
 		self.current_question = self.current_question + 1
@@ -82,19 +86,19 @@ function MultipleChoiceView:press(key)
 		else
 			self.quiz_state = "IDLE"
 		end
-
 		self:dirty(true)
-	-- display final result
 	elseif key=="right" and self.end_flag==1 then
+		-- Quiz is finished. Set up for a final result screen
 		self.mult_choice_quiz.questions:calculate_score(correct_answer_number)
 		self.quiz_state = "DONE"
 		self:dirty(true)
 	elseif key == "back" then
 		self:trigger("exit")
 	else
-		--get user answer
-		if self.user_input ~= nil and #self.user_input<=3 then
-			if(key=="1" or key=="2" or key=="3" or key=="4") then
+		--Check if the user input can be interpreted as a answer and in that case
+		-- append it to the current answer
+		if self.user_input ~= nil and #self.user_input <= 3 then
+			if(key == "1" or key == "2" or key == "3" or key == "4") then
 				self.user_input = self.user_input .. key
 			end
 		end
@@ -104,10 +108,9 @@ end
 --Renders this instance of MultipleChoiceView and all its child views, given
 -- that it's flagged as dirty
 function MultipleChoiceView:render(surface)
-	print(self.quiz_state)
 	if not self.listening_initiated then
-		-- This view are not listening to all the components it should
-		-- listen to yet
+		-- If this view are not listening to all the components it should
+		-- listen to yet: start listening
 		local callback = utils.partial(self.press)
 		self:listen_to(
 			event.remote_control,
@@ -119,25 +122,38 @@ function MultipleChoiceView:render(surface)
 	end
 
 	if self:is_dirty() then
-		surface:clear(color)
+		-- If the view is marked as dirty, re-render it
 		if self.quiz_state == "IDLE" then
 			surface:clear(color)
+			--Buttons
 			local buttonColor = {r=0, g=128, b=225}
 			local textColor = {r=0, g=0, b=0}
-			local choiceButton1 = sys.new_freetype(textColor, 30, {x=100,y=400}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-			local choiceButton2 = sys.new_freetype(textColor, 30, {x=350,y=400}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-			local choiceButton3 = sys.new_freetype(textColor, 30, {x=600,y=400}, utils.absolute_path("data/fonts/DroidSans.ttf"))
-			local choiceButton4 = sys.new_freetype(textColor, 30, {x=850,y=400}, utils.absolute_path("data/fonts/DroidSans.ttf"))
+			local choiceButton1 = sys.new_freetype(textColor, 30, {x=100,y=400},
+				utils.absolute_path("data/fonts/DroidSans.ttf"))
+			local choiceButton2 = sys.new_freetype(textColor, 30, {x=350,y=400},
+				utils.absolute_path("data/fonts/DroidSans.ttf"))
+			local choiceButton3 = sys.new_freetype(textColor, 30, {x=600,y=400},
+				utils.absolute_path("data/fonts/DroidSans.ttf"))
+			local choiceButton4 = sys.new_freetype(textColor, 30, {x=850,y=400},
+				utils.absolute_path("data/fonts/DroidSans.ttf"))
 
-			self.font:draw_over_surface(surface, self.current_question .. "." .. self.mult_choice_quiz.questions[self.current_question].question)
+			-- Draw question
+			self.font:draw_over_surface(surface, self.current_question ..
+				"." .. self.mult_choice_quiz.questions[self.current_question].question)
+
+			--Button text
 			surface:fill(buttonColor, {width=200, height=60, x=100, y=400})
-			choiceButton1:draw_over_surface(surface,"(1)." .. self.mult_choice_quiz.questions[self.current_question].Choices[1])
+			choiceButton1:draw_over_surface(surface,"(1)." ..
+			 	self.mult_choice_quiz.questions[self.current_question].Choices[1])
 			surface:fill(buttonColor, {width=200, height=60, x=350, y=400})
-			choiceButton2:draw_over_surface(surface,"(2)." .. self.mult_choice_quiz.questions[self.current_question].Choices[2])
+			choiceButton2:draw_over_surface(surface,"(2)." ..
+				self.mult_choice_quiz.questions[self.current_question].Choices[2])
 			surface:fill(buttonColor, {width=200, height=60, x=600, y=400})
-			choiceButton3:draw_over_surface(surface,"(3)." .. self.mult_choice_quiz.questions[self.current_question].Choices[3])
+			choiceButton3:draw_over_surface(surface,"(3)." ..
+				self.mult_choice_quiz.questions[self.current_question].Choices[3])
 			surface:fill(buttonColor, {width=200, height=60, x=850, y=400})
-			choiceButton4:draw_over_surface(surface,"(4)." .. self.mult_choice_quiz.questions[self.current_question].Choices[4])
+			choiceButton4:draw_over_surface(surface,"(4)." ..
+				self.mult_choice_quiz.questions[self.current_question].Choices[4])
 
 		elseif self.quiz_state == "DISPLAY_RESULT" then
 			-- Display the result from one question
@@ -147,7 +163,9 @@ function MultipleChoiceView:render(surface)
 		elseif self.quiz_state == "DONE" then
 			-- Display the result from the whole quiz
 			surface:clear(color)
-			self.font:draw_over_surface(screen, "You answered " .. self.correct_answer_number .. " questions correctly and your score is " .. self.mult_choice_quiz:get_score() .. ".")
+			self.font:draw_over_surface(screen, "You answered "
+			.. self.correct_answer_number .. " questions correctly and your score is "
+			.. self.mult_choice_quiz:get_score() .. ".")
 		end
 	end
 	-- TODO Render all child views and copy changes to this view
