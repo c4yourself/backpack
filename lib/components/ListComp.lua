c533f5536b65d765485590b195b78e7f2d0d0de9
Andreas Kozma (Line Manager Sales) <andko220@student.liu.se>
2015-11-12 16:17:47 +0100

    Modified ListItem and ListComp

--- lib/components/ListComp.lua
+++ lib/components/ListComp.lua
@@ -9,25 +9,27 @@ local ListComp = class("ListComp",View)
 
 function ListComp:__init()
 	View.__init(self)
-	self.item_list = {} -- a list contains all ListItems for the view.
+
+	self.item_list = {} 				-- a list contains all ListItems for the view.
 	self.start_indicator = true -- will function just once, help to map the indicator with
 															-- the selected ListItem when the ListComp is created.
 	self.list_indicator = nil
 
 end
 
-function ListComp:add_list_item(list_item) --ListItem_size behövs den? och button=ListItem? Här vill vi kontrollera att ListComp inte går utanför TravelViews surface
+function ListComp:add_list_item(list_item)
 	 table.insert(self.item_list, list_item)
 end
 
-
-
 function ListComp:render(surface)
+	surface:clear({0, 255, 0, 255})
 
+--[[
 	if self.start_indicator == true then
 		for k = 1 , #self.item_list do
-			if self.item_list[k].listItem:is_selected() then
-				self.listItem_indicator = k
+			print(self.item_list[1])
+			if self.item_list[k]:is_selected() then
+				self.list_indicator = k
 			end
 		end
 
@@ -38,35 +40,25 @@ function ListComp:render(surface)
 
 		self.start_indicator = false
 	end
+]]
+-- Go through the item_list to render all list_items
 
--- Go through the item_list to render all buttons
-    for i=1 , #self.item_list do
-		local list_data = self.button_list[i]
+		local height = 30
+		for i=1 , #self.item_list do
+		local list_data = self.item_list[i]
 		local area = {
-			width = list_data.width,
-			height	=list_data.height,
 			x = list_data.x,
 			y = list_data.y
 		}
 
-		local sub_surface = SubSurface(surface,area)
-			list_data.listItem:render(sub_surface)
-		if list_data.listItem.text_available then
-			self:display_text(surface, i)
-		end
+		local sub_surface = SubSurface(surface, {
+			x = 0, y = (i - 1) * height, width = surface:get_width(), height = height})
+		list_data:render(sub_surface)
+--		if list_data.listItem.text_available then
+--			self:display_text(surface, i)
+--		end
 	end
-
 end
-
-end
-
-
-
-
-
-
-
-
 
 
 return ListComp
--- lib/components/ListItem.lua
+++ lib/components/ListItem.lua
@@ -3,18 +3,18 @@
 local class = require("lib.classy")
 local View = require("lib.view.View")
 local ListItem = class("ListItem", View)
+local Font = require("lib.font.Font")
+local Color = require("lib.draw.Color")
 
 
-function ListItem:__init(text_left, text_position_left, text_color, text_color_selected, enabled, selected)
+function ListItem:__init(text_left, font, text_position_left, text_color_selected, enabled, selected)
 	View.__init(self)
 
 	--text_right, font_size, font_path, icon
-
-
 	self.text_left = text_left
 	self.text_position_left = text_position_left
 	--self.text_right = text_right
-  self.text_color = text_color
+  self.font = font
 	self.text_color_selected = text_color_selected or text_color
  	self._enabled = enabled or true
   self._selected = selected or false
@@ -24,46 +24,13 @@ function ListItem:__init(text_left, text_position_left, text_color, text_color_s
 --  self.icon = icon
 end
 
-function ListItem:enable(status)
-	if status == nil then
-		status = true
-	end
-
-	local old_status = self._enabled
-	self._enabled = status
-
-	self:is_dirty()
-end
-
-function ListItem:select(status)
-	if status == nil then
-		status = true
-	end
-
-	local old_status = self._selected
-	self._selected = status
-	self:is_dirty(true)
-end
-
-function ListItem:is_selected()
-	return self._selected
-end
-
-
 function ListItem:render(surface)
+
+surface:clear({r=255, g=255, b=255, a=255})
+self.font:draw(surface, self.text_position_left, self.text_left)
 
 self:dirty(false)
 
-if not self:is_enabled() then
-	surface:clear(self.text_color:to_table())
-elseif self:is_selected() then
-	surface:clear(self.text_color_selected:to_table())
-else
-	surface:clear(self.text_color:to_table())
 end
-
-end
-
-
 
 return ListItem
