67b0a637e4c8b3d4c38eff5229c2a2db3845e2ed
Fredrik Lofberg (testleader) <frelo206@student.liu.se>
2015-11-12 14:24:14 +0100

    changed start to a simpler file to test on the box

---
+++ start_real.lua
@@ -0,0 +1,42 @@
+local event = require("lib.event")
+local logger = require("lib.logger")
+local utils = require("lib.utils")
+local menu = require("views.menu")
+local view = require("lib.view")
+local CityView = require("views.CityView")
+local CityView2 = require("views.CityView2")
+
+--- This function runs every time a key is pressed
+-- The current mapping for the emulator can be found in emulator/zto.lua
+-- @param key Key that was pressed
+-- @param state Either up or repeat
+function onKey(key, state)
+	logger.trace("Remote control input (" .. key .. ", " .. state .. ")")
+
+	--testing remote control
+	if state == "down" then
+		event.remote_control:trigger("button_press", key)
+	elseif state == "up" then
+		event.remote_control:trigger("button_release", key)
+	else
+		event.remote_control:trigger("button_repeat", key)
+	end
+
+	-- Terminate program when exit key is pressed
+	if key == "exit" and state == "up" then
+		sys.stop()
+	end
+end
+
+-- This function is called at the start of the program
+function onStart()
+  --local city_view = CityView(event.remote_control)
+	--view.view_manager:set_view(city_view)
+
+	-- the "up" and "down" buttons are enabled for
+	-- choosing alternatives in city_view_2
+	local city_view_2 = CityView2(event.remote_control)
+	view.view_manager:set_view(city_view_2)
+
+	gfx.update()
+end
