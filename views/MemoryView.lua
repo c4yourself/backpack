80ffdd940b8a5f0c32d4f6b78dd1f072ef539cca
Linnea <linbe712@student.liu.se>
2015-11-13 13:19:07 +0100

    Prints cards in square, this should work but something else is wrong

--- views/MemoryView.lua
+++ views/MemoryView.lua
@@ -13,6 +13,8 @@ local MemoryView = class("MemoryView", View)
 local card= require("lib.components.MemoryCardComponent")
 local button_grid	=	require("lib.components.ButtonGrid")
 local color = require("lib.draw.Color")
+local serpent = require("lib.serpent")
+local button = require("lib.components.Button")
 
 
 function MemoryView:__init()
@@ -51,72 +53,107 @@ end
 
 --
 function MemoryView:press(key)
-  if key == "right" or key == "up" or key == "down" or key == "left" then
-		-- change coloir on card to visualize a move
-		--if self.player_moved then
-      self.player_moved = true
-			self:dirty(true)
-			--view.view_manager:render()
-	--	end
-	elseif key == "back" then
+print("presssssssss")
+print(key)
+if key == "down" then
+  print("down1")
+  -- the indicator refers to the selecting button
+  local indicator = self.buttonGrid.button_indicator
+  local button_list = self.buttonGrid.button_list
+
+  indicator = indicator % #button_list + 1
+
+  button_list[indicator].button:select(true)
+
+  if indicator == 1 then
+    button_list[#button_list].button:select(false)
+  else
+    button_list[indicator-1].button:select(false)
+  end
+
+  self.buttonGrid.button_indicator = indicator
+
+  self:dirty(true)
+  --gfx.update()
+elseif key == "up" then
+  print("up2")
+  local indicator = self.buttonGrid.button_indicator
+  local button_list = self.buttonGrid.button_list
+  indicator = indicator - 1
+  if indicator == 0 then
+    indicator = #self.buttonGrid.button_list
+  end
+  button_list[indicator].button:select(true)
+
+  if indicator == #button_list then
+    button_list[1].button:select(false)
+  else
+    button_list[indicator+1].card:select(false)
+  end
+
+  self.buttonGrid.button_indicator = indicator
+  self:dirty(true) --use the view's button grid for rendering
+
+  --gfx.update()
+-- end
+--   --if key == "right" or key == "up" or key == "down" or key == "left" then
+-- 		-- change coloir on card to visualize a move
+-- 		if self.player_moved then
+--       self.player_moved = true
+-- 			self:dirty(true)
+--       self.cards[2]:select()
+--       self.buttonGrid:render(surface)
+-- 			--view.view_manager:render()
+-- 		end
+elseif key == "back" then
 		--TODO find a way to create the correct city view
 		self:trigger("exit")
-	end
+end
+end
+
+function MemoryView:_print_board()
+  -- Colors for buttons
+    local button_color = color(0, 128, 225, 255)
+    local color_selected = color(33, 99, 111, 255)
+    local color_disabled = color(111,222,111,255)
+    local text_color = color(55, 55, 55, 255)
+
+    local button_size = {width = 25, height = 25}
+  self.buttonGrid = button_grid()
+  self.pos_x = 450
+  self.pos_y = 50
+  local columns = math.ceil((self.pairs*2)^(1/2))
+  for i = 1, self.pairs*2 do
+    self.cards[i]  = button(button_color, color_selected, color_disabled, true, false)
+    if i == 1 then
+      self.pos_x = self.pos_x
+    elseif ((i-1) % columns == 0) then
+      self.pos_y = self.pos_y + 50
+      self.pos_x = self.pos_x - (columns - 1) * 50
+    else
+      self.pos_x = self.pos_x + 50
+    end
+  self.positions[i] = {x = self.pos_x, y = self.pos_y}
+  self.buttonGrid:add_button(self.positions[i], button_size, self.cards[i])
+  end
+  self:dirty(true)
+  --self.buttonGrid:render(surface)
 end
 
 --Renders MemoryView and all of its child views
 function MemoryView:render(surface)
---surface:clear({255, 0, 0, 255})
-
--- Make screen red when clicking a button
---  surface:clear({255, 0, 0, 255})
-
--- Colors for buttons
-  local button_color = color(0, 128, 225, 255)
-  local color_selected = color(33, 99, 111, 255)
-  local color_disabled = color(111,222,111,255)
-  local text_color = color(55, 55, 55, 255)
-
--- Button instance
-
- local pos = {x=50, y=450}
- self.pos_x = 50
- self.pos_y = 450
-for i = 1, self.pairs*2 do
-  self.cards[i]  = card(button_color, color_selected, color_disabled, true, false, self.memory.cards[i])
-  self.pos_x = self.pos_x + 50
-  pos = {x = self.pos_x, y = self.pos_y}
-  self.positions[i] = pos
-end
-
-  local button_size = {width = 25, height = 50}
-
--- Create button grid
-  self.buttonGrid = button_grid()
-  for i = 1, self.pairs*2 do
-  self.buttonGrid:add_button(self.positions[i], button_size, self.cards[i])
-  end
-
--- Insert text and color to each buttonGrid
---  card_1:set_textdata("Back to City", text_color, {x = 100, y = 450}, 30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-
-  self.buttonGrid:render(surface)
-
+-- Create cards and cardgrid using buttonGrid
+-- Draws the board depending on the chosen abount of pairs
+  self:_print_board()
 
 if not self.listening_initiated then
   local change_callback = utils.partial(self.render, self, surface)
-
   self.listening_initiated = true
 end
 
 if self:is_dirty() then
-  print("in dirty")
-  surface:clear(color)
-if self.player_moved then -- The user has moved to another card
-    color = {0, 0, 255, 255}
-    surface:clear(color)
-    self.font:draw_over_surface(surface, "hej")
-end
+  --print("in dirty")
+  --surface:clear(color)
 end
 
 --If the user updated dirty flag will remain true to make sure the user can
