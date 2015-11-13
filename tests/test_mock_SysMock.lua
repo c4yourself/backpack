1a4b3996e8a61a176c11d260ba7c06851c31ae67
Fredrik Lofberg (testleader) <frelo206@student.liu.se>
2015-11-13 17:13:46 +0100

    Fixed SysMock, changed in Gfx mock and also changed in their testing programs, sys is still not done

--- tests/test_mock_SysMock.lua
+++ tests/test_mock_SysMock.lua
@@ -1,7 +1,25 @@
 local class = require("lib.classy")
 --local PlayerMock = require("lib.mocks.PlayerMock")
 local luaunit = require("luaunit")
+local SysMock = require("lib.mocks.SysMock")
+local PlayerMock = require("lib.mocks.PlayerMock")
 
 local TestSysMock = {}
---TODO write tests
+
+--Sets up an instans of the object SysMock before each test
+function TestSysMock:setUp()
+	self.sys = SysMock()
+end
+
+--Tests if it returns the fixed time 2355
+function TestSysMock:test_time()
+	luaunit.assertEquals(self.sys:time(), 2355)
+end
+
+--Tests if new_player returns a type player
+function TestSysMock:test_new_player()
+	local player = PlayerMock()
+	luaunit.assertEquals(type(self.sys:new_player()), type(player))
+end
+
 return TestSysMock
