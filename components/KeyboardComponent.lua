ac88b67809565ad3666ebc0f84e26420eda40678
Filip Claesson <filcl133@student.liu.se>
2015-11-14 13:44:56 +0100

    effective print out buttons

--- components/KeyboardComponent.lua
+++ components/KeyboardComponent.lua
@@ -26,56 +26,30 @@ function KeyboardComponent:render(surface)
 	local color_disabled = color(111, 222, 111, 255) --have not been used yet
 
 	-- Instance of	all Buttons
-	local button_1 = button(button_color, color_selected, color_disabled,true,true)
-	local button_2 = button(button_color, color_selected, color_disabled,true,false)
-	local button_3 = button(button_color, color_selected, color_disabled,true,false)
-	local button_4 = button(button_color, color_selected, color_disabled,true,false)
-	local button_5 = button(button_color, color_selected, color_disabled,true,false)
-	local button_6 = button(button_color, color_selected, color_disabled,true,false)
-	local button_7 = button(button_color, color_selected, color_disabled,true,false)
-	local button_8 = button(button_color, color_selected, color_disabled,true,false)
-	local button_9 = button(button_color, color_selected, color_disabled,true,false)
-
- -- Define each button's posotion and size
-	local position_1={x=50,y=50}
-	local button_size= {width=50,height=50}
-	local position_2={x=110,y=50}
-	local position_3={x=170,y=50}
-	local position_4={x=50,y=110}
-	local position_5={x=110,y=110}
-	local position_6={x=170,y=110}
-	local position_7={x=50,y=170}
-	local position_8={x=110,y=170}
-	local position_9={x=170,y=170}
-
-	-- create a button grid for the current view,
-	-- for managing all buttons' layout and states
+	buttons = {}
+	positions = {}
+	letters={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z","@","-","_","1","2","3","4","5","6","7","8","9","0"}
+	mx = 0
+	my = 0
+	local button_start_location = {left=50, top=100}
+	local button_size= {width=60,height=60}
+	local button_padding = 10
+	local nbr_of_buttons = {x=6, y=6}
 	self.buttonGrid = button_grid()
-
-	-- Using the button grid to create buttons
-	self.buttonGrid:add_button(position_1,button_size,button_1)
-	self.buttonGrid:add_button(position_2,button_size,button_2)
-	self.buttonGrid:add_button(position_3,button_size,button_3)
-	self.buttonGrid:add_button(position_4,button_size,button_4)
-	self.buttonGrid:add_button(position_5,button_size,button_5)
-	self.buttonGrid:add_button(position_6,button_size,button_6)
-	self.buttonGrid:add_button(position_7,button_size,button_7)
-	self.buttonGrid:add_button(position_8,button_size,button_8)
-	self.buttonGrid:add_button(position_9,button_size,button_9)
-
-	-- Insert text and its color, position, size, path for each button
-	-- Button always has the screen as its background, not its subsurface
-	button_1:set_textdata("a",text_color,{x=60,y=60},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_2:set_textdata("b",text_color,{x=120,y=60},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_3:set_textdata("c",text_color,{x=180,y=60},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_4:set_textdata("d",text_color,{x=60,y=120},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_5:set_textdata("e",text_color,{x=120,y=120},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_6:set_textdata("f",text_color,{x=180,y=120},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_7:set_textdata("g",text_color,{x=60,y=180},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_8:set_textdata("h",text_color,{x=120,y=180},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
-	button_9:set_textdata("i",text_color,{x=180,y=180},30,utils.absolute_path("data/fonts/DroidSans.ttf"))
+	for i=1, nbr_of_buttons["x"]*nbr_of_buttons["y"] do
+		buttons[i] = button(button_color, color_selected, color_disabled,true,true)
+		positions[i] = {x=(button_start_location["left"]+((button_size["width"]+button_padding)*mx)), y=(button_start_location["top"]+((button_size["height"]+button_padding)*my))}
+		buttons[i]:set_textdata(letters[i],text_color,{x=(button_start_location["left"]+((button_size["width"]+button_padding)*mx))+button_size["height"]/4, y=(button_start_location["top"]+((button_size["height"]+button_padding)*my))+button_size["height"]/4},button_size["height"]/2,utils.absolute_path("data/fonts/DroidSans.ttf"))
+		mx = mx+1
+		if (i%nbr_of_buttons["x"]==0) then
+			mx = 0
+			my = my+1
+		end
+		self.buttonGrid:add_button(positions[i],button_size,buttons[i])
+	end
 
 	self.buttonGrid:render(surface)
+
 end
 
 function KeyboardComponent:button_press(button)
