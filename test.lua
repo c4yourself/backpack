92c2f3d4277a69e1b4e070252f93bad13abd1376
Fredrik Vendelström (Deploymentmanager) <freve985@student.liu.se>
2015-11-17 10:37:39 +0100

    Improved computer AI

--- test.lua
+++ test.lua
@@ -1,6 +1,13 @@
 local connectfour = require("lib.connectfour")
 local a = connectfour.ConnectFour()
 
+--print(a:serialize())
+
+local b = connectfour.ConnectFour().unserialize("~~~~~~~\nX~~~~~~\nO~~~~~~\nX~~~~~~\nO~~~~~~\nX~~~~~~\n")
+print("~~~~~~~\nX~~~~~~\nO~~~~~~\nX~~~~~~\nO~~~~~~\nX~~~~~~\n")
+print(b:serialize())
+
+a:move("X", 5)
 print(a:serialize())
 
 --[[] a:move("X", 4)
@@ -31,9 +38,11 @@ a:move("X", 7)
 print(a:serialize()) --]]
 
 
-a:move("X", 7)
-print(a:serialize())
-a:move("O", 7)
+--a:move("X", 7)
+--print(a:serialize())
+
+
+--[[a:move("O", 7)
 print(a:serialize())
 a:move("X", 6)
 print(a:serialize())
@@ -46,7 +55,7 @@ print(a:serialize())
 a:move("X", 4)
 print(a:serialize())
 a:move("O", 4)
-print(a:serialize())
+print(a:serialize())--]]
 
 --[[print(a:get(6,4))
 print(a:get(2,3))
