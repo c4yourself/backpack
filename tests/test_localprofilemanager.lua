02704522d3f80696f4770e6c686214d3ce3c7e66
Huanyu Li <huali592@student.liu.se>
2015-11-17 14:55:05 +0100

    write unit test of localprofilemanager

---
+++ tests/test_localprofilemanager.lua
@@ -0,0 +1,11 @@
+local luaunit = require("luaunit")
+local class = require("lib.classy")
+local Profile = require("lib.profile.Profile")
+local localprofilemanager = require("lib.profile.localprofilemanager")
+local Testlocalprofilemanager = {}
+
+
+function Testlocalprofilemanager:setUp()
+  self.profile = Profile("HuanyuLi","lihuanyuasas@163.com","1992-06-29","male","lodon")
+end
+return TestProfile
