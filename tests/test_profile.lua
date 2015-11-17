f7e794f22f7bc14c57050df9bdda11aa8c3e4731
Huanyu Li <huali592@student.liu.se>
2015-11-17 14:22:01 +0100

    update unit test of Profile

--- tests/test_profile.lua
+++ tests/test_profile.lua
@@ -6,9 +6,17 @@ local TestProfile = {}
 
 function TestProfile:setUp()
 
-  self.profile = Profile("HuanyuLi","lihuanyuasas@163.com","1992-06-29","male")
+  self.profile = Profile("HuanyuLi","lihuanyuasas@163.com","1992-06-29","male","lodon")
 end
-
+function TestProfile:test_get_name()
+  luaunit.assertEquals(self.profile:get_name(),"HuanyuLi")
+end
+function TestProfile:test_get_profile_name()
+  luaunit.assertEquals(self.profile:get_profile_name(),"lodon__lihuanyuass@163.com")
+end
+function TestProfile:test_get_email_address()
+  luaunit.assertEquals(self.profile:get_email_address(),"lihuanyuass@163.com")
+end
 function TestProfile:test_set_balance()
   luaunit.assertEquals(self.profile:set_balance(100), self.profile.balance)
 end
