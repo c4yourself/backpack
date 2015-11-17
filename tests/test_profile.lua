4ddc2c5b8eee83b41f04ace60e401c90233394ed
Huanyu Li <huali592@student.liu.se>
2015-11-17 14:50:23 +0100

    write unit test of profile

--- tests/test_profile.lua
+++ tests/test_profile.lua
@@ -5,17 +5,33 @@ local TestProfile = {}
 
 
 function TestProfile:setUp()
-
   self.profile = Profile("HuanyuLi","lihuanyuasas@163.com","1992-06-29","male","lodon")
 end
 function TestProfile:test_get_name()
   luaunit.assertEquals(self.profile:get_name(),"HuanyuLi")
 end
 function TestProfile:test_get_profile_name()
-  luaunit.assertEquals(self.profile:get_profile_name(),"lodon__lihuanyuass@163.com")
+  luaunit.assertEquals(self.profile:get_profile_name(),"lodon__lihuanyuasas@163.com")
 end
 function TestProfile:test_get_email_address()
-  luaunit.assertEquals(self.profile:get_email_address(),"lihuanyuass@163.com")
+  luaunit.assertEquals(self.profile:get_email_address(),"lihuanyuasas@163.com")
+end
+function TestProfile:test_get_date_of_birth()
+  luaunit.assertEquals(self.profile:get_date_of_birth(),"1992-06-29")
+end
+function TestProfile:test_get_sex()
+  luaunit.assertEquals(self.profile:get_sex(),"male")
+end
+function TestProfile:test_get_city()
+  luaunit.assertEquals(self.profile:get_city(),"lodon")
+end
+function TestProfile:test_set_badges()
+  self.profile:set_badges("[1,3,2]")
+  luaunit.assertEquals(self.profile:get_badges(),{1,3,2})
+end
+function TestProfile:test_set_inventory()
+  self.profile:set_inventory("{\"1\": 1,\"2\": 111,\"3\": 12}")
+  luaunit.assertEquals(self.profile:get_inventory(),{1,111,12})
 end
 function TestProfile:test_set_balance()
   luaunit.assertEquals(self.profile:set_balance(100), self.profile.balance)
