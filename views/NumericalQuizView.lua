06d7a87d34c52eff23b5f6c2714c610d65b4b7ea
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-11-22 22:45:30 +0100

    Refactored NumericalQuizView after adding the progress bar

--- views/NumericalQuizView.lua
+++ views/NumericalQuizView.lua
@@ -28,12 +28,13 @@ function NumericQuizView:__init()
 	self.listening_initiated = false
 	self.input_area_defined = false
 	self.areas_defined = false
-	self.prevent = false
+	self.prevent = false -- This flag is used for avoiding bug where render is
+						-- called twice upon a submit.
 	--Components
 	self.grid = NumericalQuizGrid(remote_control)
 	--Instanciate a numerical input component and make the quiz listen for changes
 	self.views.num_input_comp = NumericalInputComponent()
-	--self.input_surface = Surface(300,100)
+
 	--Button data
 	local button_color = Color(255, 99, 0, 255)
 	local color_selected = Color(255, 153, 0, 255)
@@ -42,6 +43,7 @@ function NumericQuizView:__init()
 	local height = screen:get_height()
 	local width = screen:get_width()
 	local button_size = {width = 372, height = 107}
+
 	-- Add exit button
 	local button_exit = Button(button_color, color_selected, color_disabled,
 								true, true, "views.CityView")
@@ -65,6 +67,7 @@ function NumericQuizView:__init()
 						button_next)
 	local next_index = self.grid:get_last_index()
 	self.grid:mark_as_next_button(next_index)
+
 	-- Logic
 	-- Associate a quiz instance with the View
 	self.num_quiz = Quiz()
@@ -73,13 +76,13 @@ function NumericQuizView:__init()
 	for i=1, #self.num_quiz.questions do
 		self.progress_table[i] = -1
 	end
-
-	-- User input
 	self.user_answer = ""
 
+	-- Background colors for this view's own subsurfaces
 	self.question_area_color = Color(255,0,0,255)
 	self.progress_counter_color = Color(0,255,0,255)
-	self.question_area_font = Font("data/fonts/DroidSans.ttf", 32, Color(255,255,255,255))
+	self.question_area_font = Font("data/fonts/DroidSans.ttf", 32,
+									Color(255,255,255,255))
 	-- Listeners and callbacks
 	self:listen_to(
 		event.remote_control,
@@ -88,19 +91,21 @@ function NumericQuizView:__init()
 	)
 end
 
---Responds to a button press when the View is active (i.e. current View for the
--- global @{ViewManager} instance)
+---Responds to a button press when the View is active
+-- @param key Key that was pressed
 function NumericQuizView:press(key)
 	if key == "back" then
 		self:trigger("exit")
 	end
 end
 
---Renders a NumericQuizView and all its child views
+---Renders a NumericQuizView and all its child views
+--@param surface Surface or SubSurface to render upon
 function NumericQuizView:render(surface)
 	local surface_width = surface:get_width()
 	local surface_height = surface:get_height()
 
+	-- Define input area if it hasn't been done already
 	if not self.input_area_defined then
 		local input_x = math.ceil(surface:get_width() * 0.4)
 		local input_y = math.ceil(surface:get_height() * 0.6)
@@ -116,16 +121,21 @@ function NumericQuizView:render(surface)
 		self.grid:mark_as_input_comp(input_index)
 	end
 
+	-- Render the view as long as it isn't clean already
 	if self:is_dirty() then
+		local color = nil -- Background color for the quiz
 		surface:clear(color)
+		-- Define other areas if it hasn't been done already
 		if not self.areas_defined then
 			--Progress counter
 			local progress_margin = 26
 			self.counter_width = 72
 			self.counter_height = 72
-			self.x_counter = math.ceil(surface_width - progress_margin - self.counter_width)
+			self.x_counter = math.ceil(surface_width - progress_margin -
+										self.counter_width)
 			self.y_counter = progress_margin
-			self.progress_counter_area = SubSurface(surface, {x = self.x_counter, y = self.y_counter,
+			self.progress_counter_area = SubSurface(surface, {x = self.x_counter,
+										y = self.y_counter,
 										height = self.counter_height,
 										width = self.counter_width})
 			--Question area
@@ -145,7 +155,7 @@ function NumericQuizView:render(surface)
 		-- Question area
 		self.question_area:clear(self.question_area_color)
 
-		--Determine what should be shown on screen
+		--Determine what should be shown in the Question area
 		if self.answer_flag then
 			-- The user has answered a question
 			local current_question = self.num_quiz.current_question
@@ -163,6 +173,8 @@ function NumericQuizView:render(surface)
 				width = self.question_area_width},
 				output, "center", "middle")
 			self.answer_flag = false
+			-- The statements below was useful for finding a bug, leaving them
+			-- in case it re-occurs
 			--print("Correct answers: " .. tostring(self.num_quiz.correct_answers))
 			--print("Wrong answers: " .. tostring(self.num_quiz.wrong_answers))
 		else
@@ -179,34 +191,41 @@ function NumericQuizView:render(surface)
 				-- The user has finished the quiz
 				self.views.num_input_comp:blur()
 				self.quiz_flag = true
-				local output = "You answered " .. tostring(self.num_quiz.correct_answers)
-				.. " questions correctly."
+				local output = "You answered " ..
+								.. tostring(self.num_quiz.correct_answers)
+								.. " questions correctly."
 				self.question_area_font:draw(self.question_area, {x = 0, y = 0,
-					height = self.question_area_height,
-					width = self.question_area_width},
-					output, "center", "middle")
+								height = self.question_area_height,
+								width = self.question_area_width},
+								output, "center", "middle")
 			end
 		end
-		-- Progress counter
+		-- Render the Progress counter
 		self.progress_counter_area:clear(self.progress_counter_color)
 		local current_question = self.num_quiz.current_question
 		local quiz_length = #self.num_quiz.questions
 		self.question_area_font:draw(self.progress_counter_area,
-			{x = 0, y = 0, height = self.counter_height,
-			width = self.counter_width},
-			tostring(current_question) .. " / " .. tostring(quiz_length), "center", "middle")
-		-- Progress bar
+									{x = 0, y = 0, height = self.counter_height,
+									width = self.counter_width},
+									tostring(current_question) .. " / " ..
+									tostring(quiz_length), "center", "middle")
+		-- Render the Progress bar
 		local bar_component_width = 45
 		local bar_component_height = 45
 		local progress_bar_margin = 10
-		local bar_component_x = self.x_counter + self.counter_width - bar_component_width
-		local bar_component_y = self.y_counter + progress_bar_margin + self.counter_height
+		local bar_component_x = self.x_counter + self.counter_width -
+								bar_component_width
+		local bar_component_y = self.y_counter + progress_bar_margin +
+								self.counter_height
 		local quiz_length = #self.progress_table
+		-- Create a progress bar and color its boxes
 		for i = 1, quiz_length do
-			local progress_bar_component = SubSurface(surface, {x = bar_component_x, y = bar_component_y,
+			local progress_bar_component = SubSurface(surface,
+										{x = bar_component_x, y = bar_component_y,
 										height = bar_component_height,
 										width = bar_component_width})
 			local bar_component_color = nil
+			-- Depending on the users success: color the boxes differently
 			if self.progress_table[i] == true then
 				bar_component_color = Color(0,255,0,255)
 			elseif self.progress_table[i] == false then
@@ -215,54 +234,62 @@ function NumericQuizView:render(surface)
 				bar_component_color = Color(255, 255, 255, 255)
 			end
 			progress_bar_component:clear(bar_component_color)
-			bar_component_y = bar_component_y + progress_bar_margin + bar_component_height
+			bar_component_y = bar_component_y + progress_bar_margin +
+								bar_component_height
 		end
 		self.prevent = false
 	end
 
+	-- In case we haven't started listning to our child views: start doing so
 	if not self.listening_initiated then
-		local change_callback = utils.partial(self.views.num_input_comp.render, self.views.num_input_comp, self.input_area)
+		local change_callback = utils.partial(self.views.num_input_comp.render,
+												self.views.num_input_comp,
+												self.input_area)
+		-- Triggered when the user changes the input in the input field
 		self:listen_to(
 			self.views.num_input_comp,
 			"change",
 			change_callback
 		)
 
+		-- Triggered when the user selects a new button/field
 		self:listen_to(
 			self.grid,
 			"dirty",
 			utils.partial(self.grid.render, self.grid, surface)
 		)
 
+		-- Triggered when the user submits an answer
 		self:listen_to(
 			self.views.num_input_comp,
 			"submit",
 			utils.partial(self.show_answer, self)
 		)
 
+		-- Triggered when the next button is pressed
 		self:listen_to(
 			self.grid,
 			"next",
 			utils.partial(self.next_question, self)
 		)
 
+		-- Triggered when the exit button is pressed
 		self:listen_to(
 			self.grid,
 			"back",
 			utils.partial(self.trigger, self, "exit")
 		)
 
-		--self.views.num_input_comp:focus() --TODO Move this to its proper place
 		self.listening_initiated = true
 	end
 
-	--surface:copyfrom(self.input_surface)
+	self:dirty(false)
 	self.grid:render(surface)
 	gfx.update()
-	self:dirty(false)
 end
 
--- Displays the correct answer and whether the user chose the correct one.
+--- Set up for showing whether the user answered correctly or not. Only works
+-- when the user has entered an answer in the input field. Triggers dirty
 function NumericQuizView:show_answer()
 	if self.views.num_input_comp:get_text() ~= "" then
 		if not self.prevent then
@@ -276,7 +303,7 @@ function NumericQuizView:show_answer()
 	end
 end
 
--- Set up for the next question in the quiz
+--- Set up for the next question in the quiz. Triggers dirty
 function NumericQuizView:next_question()
 	self.answer_flag = false
 	self.prevent = false
