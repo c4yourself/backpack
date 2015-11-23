e07796f627d7a01777a20d0d042ee0c383a75de4
Robert Krogh <robkr627@student.liu.se>
2015-11-23 15:23:35 +0100

    Speeded up the drawing by not letting ButtonGrid draw the text

--- components/KeyboardComponent.lua
+++ components/KeyboardComponent.lua
@@ -3,11 +3,11 @@ local event = require("lib.event")
 local class = require("lib.classy")
 local View = require("lib.view.View")
 local view = require("lib.view")
-local KeyboardComponent = class("KeyboardComponent", View)
 local logger = require("lib.logger")
-local button = require("lib.components.TextButton")
+local button = require("lib.components.Button")
 local button_grid	=	require("lib.components.ButtonGrid")
 local color = require("lib.draw.Color")
+local KeyboardComponent = class("KeyboardComponent", View)
 
 KeyboardComponent.active = true
 -- Constructor for SplashView
@@ -15,7 +15,7 @@ KeyboardComponent.active = true
 function KeyboardComponent:__init(remote_control)
 	View.__init(self)
 	self.background_path = ""
-	local TextButton_color = color(0, 128, 225, 255)
+	local button_color = color(0, 128, 225, 255)
 	local text_color = color(55, 55, 55, 255)
 	local score_text_color = color(255, 255, 255, 255)
 	local color_selected = color(33, 99, 111, 255)
@@ -29,23 +29,36 @@ function KeyboardComponent:__init(remote_control)
 	mx = 0
 	my = 0
 
-	local button_size= {width=90,height=70}
-	local button_padding = 10
+	self.button_size= {width=90,height=70}
+	self.button_padding = 10
 	self.nbr_of_buttons = {x=5, y=7}
 
-	local button_start_location = {left=((screen:get_width()/2)-(self.nbr_of_buttons["x"]*(button_size["width"]+button_padding)))/2, top=100}
+	self.button_start_location = {left=((screen:get_width()/2)-(self.nbr_of_buttons["x"]*(self.button_size["width"]+self.button_padding)))/2, top=100}
 	self.buttonGrid = button_grid()
 	for i=1, self.nbr_of_buttons["x"]*self.nbr_of_buttons["y"] do
-		local temp_button = button(utils.absolute_path("data/fonts/DroidSans.ttf"), button_size["height"]/2, true,false)
-		positions[i] = {x=(button_start_location["left"]+((button_size["width"]+button_padding)*mx)), y=(button_start_location["top"]+((button_size["height"]+button_padding)*my))}
-		temp_button:set_textdata(self.letters[i])
+		local temp_button = button(
+			 button_color,
+			 color_selected,
+			 color_disabled,
+			 true,
+			 false)
+
+		positions[i] = {x=(self.button_start_location["left"]+((self.button_size["width"]+self.button_padding)*mx)),
+		y=(self.button_start_location["top"]+((self.button_size["height"]+self.button_padding)*my))}
+
+		-- temp_button:set_textdata(self.letters[i],text_color,
+		-- {x=(self.button_start_location["left"]+((self.button_size["width"]+self.button_padding)*mx))+self.button_size["height"]/2,
+		--  y=(self.button_start_location["top"]+((self.button_size["height"]+self.button_padding)*my))+self.button_size["height"]/4},
+		--  self.button_size["height"]/2,utils.absolute_path("data/fonts/DroidSans.ttf"))
+
+
 
 		mx = mx+1
 		if (i%self.nbr_of_buttons["x"]==0) then
 			mx = 0
 			my = my+1
 		end
-		self.buttonGrid:add_button(positions[i],button_size,temp_button)
+		self.buttonGrid:add_button(positions[i],self.button_size,temp_button)
 	end
 
 
@@ -59,7 +72,26 @@ function KeyboardComponent:render(surface)
 
 	self.buttonGrid:render(surface)
 
+	mx = 0
+	my = 0
+
+	for i=1, self.nbr_of_buttons["x"]*self.nbr_of_buttons["y"] do
+		local spoc = sys.new_freetype({r=255, g=255, b=255}, 32,
+		{x=(self.button_start_location["left"]+((self.button_size["width"]+self.button_padding)*mx))+self.button_size["height"]/2,
+		y=(self.button_start_location["top"]+((self.button_size["height"]+self.button_padding)*my))+self.button_size["height"]/4},
+		utils.absolute_path("data/fonts/DroidSans.ttf"))
+		spoc:draw_over_surface(screen, tostring(self.letters[i]))
+
+		mx = mx+1
+		if (i%self.nbr_of_buttons["x"]==0) then
+			mx = 0
+			my = my+1
+		end
+	end
+
 	self.keyboard_position = self.buttonGrid.button_indicator
+
+
 end
 
 function KeyboardComponent:get_string()
@@ -79,7 +111,8 @@ function KeyboardComponent:button_press(button)
 			self.buttonGrid.button_list[self.keyboard_position].button:select(false)
 			self.buttonGrid.button_list[self.keyboard_position+self.nbr_of_buttons["x"]].button:select()
 			self.keyboard_position = self.keyboard_position+self.nbr_of_buttons["x"]
-			self.buttonGrid:render(self.surface)
+			--self.buttonGrid:render(self.surface)
+			self:render(self.surface)
 			gfx.update()
 		end
 	elseif button == "up" then
@@ -87,7 +120,8 @@ function KeyboardComponent:button_press(button)
 			self.buttonGrid.button_list[self.keyboard_position].button:select(false)
 			self.buttonGrid.button_list[self.keyboard_position-self.nbr_of_buttons["x"]].button:select()
 			self.keyboard_position = self.keyboard_position-self.nbr_of_buttons["x"]
-			self.buttonGrid:render(self.surface)
+			--self.buttonGrid:render(self.surface)
+			self:render(self.surface)
 			gfx.update()
 		end
 	elseif button == "right" then
@@ -95,7 +129,8 @@ function KeyboardComponent:button_press(button)
 			self.buttonGrid.button_list[self.keyboard_position].button:select(false)
 			self.buttonGrid.button_list[self.keyboard_position+1].button:select()
 			self.keyboard_position = self.keyboard_position+1
-			self.buttonGrid:render(self.surface)
+			--self.buttonGrid:render(self.surface)
+			self:render(self.surface)
 			gfx.update()
 		end
 	elseif button == "left" then
@@ -103,7 +138,8 @@ function KeyboardComponent:button_press(button)
 			self.buttonGrid.button_list[self.keyboard_position].button:select(false)
 			self.buttonGrid.button_list[self.keyboard_position-1].button:select()
 			self.keyboard_position = self.keyboard_position-1
-			self.buttonGrid:render(self.surface)
+			--self.buttonGrid:render(self.surface)
+			self:render(self.surface)
 			gfx.update()
 		end
 	elseif button == "ok" then
