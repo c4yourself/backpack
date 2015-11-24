77f83aa366efe7cb042dc5cc8cea2e42a338d2fa
Ellen Fransson (tester) <ellfr210@student.liu.se>
2015-11-24 20:10:36 +0100

    Added descriptions and name for attractions in mumbai

--- lib/attractions.lua
+++ lib/attractions.lua
@@ -3,8 +3,8 @@ local City = require("lib.city")
 local attractions = {}
 
 attractions.attraction = { paris = {
-                                    {name = "The Eiffel Tower",
-                                    pic_url = "data/images/CityTourEiffelTower.png",
+                                      {name = "The Eiffel Tower",
+                                      pic_url = "data/images/CityTourEiffelTower.png",
                                     text = {"The Eiffel Tower is named after the engineer Gustave Eiffel, whose ",
                                             "company designed and built the tower. It was finished in 1889 and",
                                             "it is a global cultural icon of France and one of the most recognisable",
@@ -67,7 +67,8 @@ attractions.attraction = { paris = {
 
                                 },
 
-                             cairo = {name = "Pyramid of Giza", pic_url = "data/images/CityTourEiffelTower.png",
+                             cairo = {
+                               {name = "Pyramid of Giza", pic_url = "data/images/CityTourEiffelTower.png",
                                     text = {"The Great Pyramid of Giza (also known as the Pyramid ",
                                     "of Khufu or the Pyramid of Cheops) is the oldest and largest of",
                                     "the three pyramids in the Giza pyramid complex bordering what is",
@@ -114,7 +115,78 @@ attractions.attraction = { paris = {
                               "Nile”. The Citadel would be the centerpiece ofthe wall. It is now a",
                               "preserved historic site, with mosques and museums."}
                             }
+                          },
 
-                              }
+                            mumbai = {
+                              {name = "Gateway of India",
+                              pic_url = "data/images/CityTourEiffelTower.png",
+                              text = {"The Gateway of India is a monument built during the British Rule",
+                              "in Mumbai City of Maharashtra state in Western India. The Gateway",
+                              "of India was built to commemorate the visit of King George V and",
+                              "Queen Mary to Mumbai, prior to the Delhi Durbar, in December",
+                              "1911. However they only got to see a cardboard model of the ",
+                              "structure since the construction did not begin till 1915. It is located",
+                              "on the waterfront in the Apollo Bunder area in South Mumbai and",
+                              "overlooks the Arabian Sea. The structure is a basalt arch, 26 meters",
+                              "(85 feet) high. It lies at the end of Chhatrapati Shivaji Marg at the",
+                              "water's edge in Mumbai Harbor. It was a crude jetty used by the ",
+                              "fishing community which was later renovated and used as a landing",
+                              "place for British governors and other prominent people. In earlier",
+                              "times, it would have been the first structure that visitors",
+                              "arriving by boat in Mumbai would have seen."}
+                            },
+                            {name = "Haji Ali Mosque/mausoleum",
+                            pic_url = "data/images/CityTourEiffelTower.png",
+                            text = {"The Haji Ali Dargah was constructed in 1431 in memory of a",
+                            "wealthy Muslim merchant, Sayyed Peer Haji Ali Shah Bukhari, who ",
+                            "gave up all his worldly possessions before making a pilgrimage to",
+                            "Mecca. Hailing  from Bukhara, in present day Uzbekistan, Bukhari",
+                            "travelled around the world in the early to mid 15th century, and",
+                            "eventually settled in present day Mumbai. According to legends",
+                            "surrounding his life, he once saw a poor woman crying on the road,",
+                            "holding an empty vessel. He asked her what the problem was, she",
+                            "sobbed that her husband would thrash her as she stumbled and ",
+                            "accidentally spilled the oil she was carrying. He asked her to take ",
+                            "him to the spot where she spilt the oil. There, he jabbed a finger ",
+                            "into the soil and the oil gushed out. The overjoyed woman filled up",
+                            "the vessel and went home. Haji Ali died during his journey to",
+                            "Mecca and miraculously the casket carrying his body, floated back",
+                            "to these shores, getting stuck in the string of rocky islets just",
+                            "off the shore of Worli. Thus, the Dargah was constructed there, on a",
+                            "tiny islet located 500 meters from the coast, in the middle of",
+                            "Worli Bay."}
+                            },
+                            {name = "Chhatrapati Shivaji Terminus",
+                            pic_url = "data/images/CityTourEiffelTower.png",
+                            text = {"Chhatrapati Shivaji Terminus (previously Victoria Terminus) is a",
+                            "UNESCO World Heritage Site and an historic railway station in",
+                            "Mumbai Maharashtra, India which serves as the headquarters of",
+                            "the Central Railways. The station took ten years to complete, the",
+                            "longest for any building of that era in Bombay. Designed by",
+                            "Frederick William Stevens with influences from Victorian Italianate",
+                            "Gothic Revival architecture and traditional Mughal buildings, the",
+                            "station was built in 1887 in the Bori Bunder area of Mumbai to",
+                            "commemorate the Golden Jubilee of Queen Victoria. Stevens earned",
+                            "the commission to construct the station after a masterpiece",
+                            "watercolor sketch by draughtsman Axel Haig. The new railway",
+                            "station was built on the location of the Bori Bunder Station and is",
+                            "one of the busiest railway stations in India."}
+                            },
+                            {name = "Taj Mahal Palace & Tower",
+                            pic_url = "data/images/CityTourEiffelTower.png",
+                            text = {"The Taj Mahal Palace Hotel is a five-star hotel located in the Colaba";
+                            "region of Mumbai, Maharashtra,India, next to the Gateway of",
+                            "India. The hotel's original building first opened its doors to guests",
+                            "on 16 December 1903. Part of the Taj Hotels, Resorts and Palaces,",
+                            "this hotel is considered the flagship property of the Tata group and",
+                            "contains 560 rooms and 44 suites and 11 restaurants. From a",
+                            "historical and architectural point of view, the two buildings that",
+                            "make up the hotel, the Taj Mahal Palace and the Tower are two",
+                            "distinct buildings, built at different times and in different",
+                            "architectural designs."}
+                            }
+                          }
+
+                        }
 
 return attractions
