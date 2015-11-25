2a9d3200ca498932e4a9cca0effb8e1840ca4b91
Fredrik Urby <freur817@student.liu.se>
2015-11-25 13:56:29 +0100

    New colors, positions and sizes to multiplechoicequiz

--- views/MultipleChoiceView.lua
+++ views/MultipleChoiceView.lua
@@ -31,7 +31,6 @@ function MultipleChoiceView:__init()
 	self.last_check = 1
 	self.quiz_state = "IDLE" -- To keep track of quiz state
 	self.areas_defined = false
-	--Components
 
 	-- Logic
 	-- Associate a quiz instance with the MultipleChoiceView
@@ -46,8 +45,8 @@ function MultipleChoiceView:__init()
 	self.answer = {}
 
 	-- Graphics and colors
-	self.question_area_color = Color(255, 0, 0, 255)
-	self.font = Font("data/fonts/DroidSans.ttf",32,Color(255,255,255,255))
+	self.question_area_color = Color(255, 255, 255, 255)
+	self.font = Font("data/fonts/DroidSans.ttf", 24, Color(0,0,0,255))
 
 	-- Buttons and grids
 	self.views.grid = MultipleChoiceGrid()
@@ -56,14 +55,14 @@ function MultipleChoiceView:__init()
 	local width = screen:get_width()
 
 	local button_color = Color(255, 99, 0, 255)
-	local color_selected = Color(255, 153, 0, 255)
+	local color_selected = Color(255, 255, 255, 55)
 	local color_disabled = Color(111, 222, 111, 255)
-	local button_size = {width = 372, height = 107}
+	local button_size = {width = 300, height = 75}
 
 	-- Add back button
-	local button_exit = Button(button_color, color_selected, color_disabled,
+	local button_exit = Button(Color(255,35,35,255), color_selected, color_disabled,
 								true, true, "views.CityView")
-	local exit_position = {x = 42, y = 500}
+	local exit_position = {x = width*0.05, y = 0.85*height}
 	button_exit:set_textdata("Back to city", Color(255,255,255,255),
 							{x = 0, y = 0}, 32,"data/fonts/DroidSans.ttf")
 	self.views.grid:add_button(exit_position,
@@ -79,10 +78,9 @@ function MultipleChoiceView:__init()
 	)
 
 	-- Add next button
-	local button_next = Button(button_color, color_selected, color_disabled,
+	local button_next = Button(Color(250, 169, 0,255), color_selected, color_disabled,
 								true, false, "")
-	local next_position = {x = width - exit_position.x - button_size.width,
-							y = exit_position.y}
+	local next_position = {x = width*0.55, y = height*0.67}
 	button_next:set_textdata("Next question", Color(255,255,255,255),
 							{x = 0, y = 0}, 32,"data/fonts/DroidSans.ttf")
 	self.views.grid:add_button(next_position,
@@ -98,18 +96,18 @@ function MultipleChoiceView:__init()
 	)
 
 	-- Add submit button
-	local button_submit = Button(button_color, color_selected, color_disabled,
+	local button_submit = Button(Color(0,102,0,255), color_selected, color_disabled,
 								true, false, "")
-	local submit_position = {x = math.ceil((width - button_size.width)/2),
-							y = exit_position.y}
-	button_submit:set_textdata("Submit", Color(255,255,255,255),
+	local submit_position = {x = width*0.25, y = height * 0.67}
+	button_submit:set_textdata("Next question", Color(255,255,255,255),
 							{x = 0, y = 0}, 32,"data/fonts/DroidSans.ttf")
 	self.views.grid:add_button(submit_position,
 						button_size,
 						button_submit)
 	local submit_index = self.views.grid:get_last_index()
-	self.views.grid:mark_as_input_comp(submit_index)
 
+	self.views.grid:mark_as_input_comp(submit_index)
+--self.views.grid:mark_as_next_button(next_index)
 	--local submit_callback = utils.partial(self.submit)
 	self:listen_to(
 		self.views.grid,
@@ -118,17 +116,12 @@ function MultipleChoiceView:__init()
 	)
 
 	-- Question buttons
-	local button_margin = 35
-	local question_button_size = {width = 200, height = 60}
-	local x_margin = math.ceil((width - 3 * button_margin - 4 * question_button_size.width)/2)
-	local button_position_1 = {x = x_margin,
-								y = 400}
-	local button_position_2 = {x = x_margin + 1 * question_button_size.width + 1 * button_margin,
-								y = 400}
-	local button_position_3 = {x = x_margin + 2 * question_button_size.width + 2 * button_margin,
-								y = 400}
-	local button_position_4 = {x = x_margin + 3 * question_button_size.width + 3 * button_margin,
-								y = 400}
+	--local button_margin = 35
+	--local x_margin = math.ceil((width - 3 * button_margin - 4 * question_button_size.width)/2)
+	local button_position_1 = {x = width*0.25, y = height*0.33}
+	local button_position_2 = {x = width*0.55, y = height*0.33}
+	local button_position_3 = {x = width*0.25, y = height*0.47}
+	local button_position_4 = {x = width*0.55, y = height*0.47}
 
 	self.question_button_1 = ToggleButton(button_color, color_selected, color_disabled,
 								true, false, "")
@@ -140,16 +133,16 @@ function MultipleChoiceView:__init()
 								true, false, "")
 
 	self.views.grid:add_button(button_position_1,
-								question_button_size,
+								button_size,
 								self.question_button_1)
 	self.views.grid:add_button(button_position_2,
-								question_button_size,
+								button_size,
 								self.question_button_2)
 	self.views.grid:add_button(button_position_3,
-								question_button_size,
+								button_size,
 								self.question_button_3)
 	self.views.grid:add_button(button_position_4,
-								question_button_size,
+								button_size,
 								self.question_button_4)
 
 	-- Listeners and callbacks
@@ -162,20 +155,17 @@ end
 
 ---Triggered everytime the user presses the submit button
 function MultipleChoiceView:_submit()
-	print("answered")
 	if self.last_check == self.current_question and self.quiz_state ~= "DONE" then
 		self.user_input = self.views.grid.input
 		for j = 1, #self.user_input, 1 do
-			print("looping")
 			if self.user_input[j] ~= nil then
 				self.answer[j] = self.user_input[j]
-				print("Answer j : " .. tostring(self.answer[j]))
 			end
 			--self.answer[j] = tonumber(string.sub(self.user_input,j,j))
 		end
 		if self.mult_choice_quiz.questions[self.current_question]:is_correct(self.answer) == true then
 			self.correct_answer_number = self.correct_answer_number + 1
-			self.result_string = "Right. You've answered "
+			self.result_string = "Correct! You've answered "
 			.. self.correct_answer_number .. " questions correctly this far."
 			self.last_check = self.last_check + 1
 		else
@@ -193,7 +183,6 @@ end
 
 ---Triggered everytime the user presses the next button
 function MultipleChoiceView:_next()
-	print("next triggered")
 	if end_flag ~= 1 then
 		-- Next question is displayed
 		-- Make sure there are questions left to display
@@ -214,7 +203,7 @@ end
 
 ---Triggered everytime the user presses the back to city button
 function MultipleChoiceView:_exit()
-	self:trigger("exit")
+	self:trigger("exit_view")
 end
 
 
@@ -279,10 +268,11 @@ function MultipleChoiceView:render(surface)
 		-- If this view are not listening to all the components it should
 		-- listen to yet: start listening
 		local callback = utils.partial(self.press)
+
 		self:listen_to(
-		event.remote_control,
-		"button_release",
-		callback
+			event.remote_control,
+			"button_release",
+			callback
 		)
 
 		self:listen_to(
@@ -300,37 +290,37 @@ function MultipleChoiceView:render(surface)
 		local surface_width = surface:get_width()
 		local surface_height = surface:get_height()
 		surface:clear(color)
+
 		-- If the areas haven't been defined yet, define them
 		if not self.areas_defined then
 			-- Question area
 			local x = math.ceil(surface:get_width() * 0.2)
-			local y = math.ceil(surface:get_height() * 0.2)
-			local question_area_width = surface_width - 2 * x
-			local question_area_height = math.ceil(0.3*surface_height)
-			self.question_area_width = question_area_width
-			self.question_area_height = question_area_height
+			local y = math.ceil(surface:get_height() * 0.1)
+			self.question_area_width = surface_width - 2 * x
+			self.question_area_height = math.ceil(0.17*surface_height)
 
 			self.question_area = SubSurface(surface, {x = x, y = y,
-				height = question_area_height,
-				width = question_area_width})
+				height = self.question_area_height,
+				width = self.question_area_width})
 
 			self.areas_defined = true
 		end
 		self.question_area:clear(self.question_area_color)
+
 		-- If the view is marked as dirty, re-render it
 		if self.quiz_state == "IDLE" then
 
 			--Buttons
-			local button_size = {width = 372, height = 107}
-			local buttonColor = {r=0, g=128, b=225}
-			local textColor = Color(0,0,0,255)
-			local choiceButton1 = Font("data/fonts/DroidSans.ttf",30,textColor)
-			local choiceButton2 = Font("data/fonts/DroidSans.ttf",30,textColor)
-			local choiceButton3 = Font("data/fonts/DroidSans.ttf",30,textColor)
-			local choiceButton4 = Font("data/fonts/DroidSans.ttf",30,textColor)
+			--local button_size = {width = 372, height = 107}
+			--local buttonColor = Color(0, 128, 225, 255)
+		--	local question_text_color = Color(0,0,0,255)
+		--	local choiceButton1 = Font("data/fonts/DroidSans.ttf", 30, question_text_color)
+		--	local choiceButton2 = Font("data/fonts/DroidSans.ttf", 30, question_text_color)
+		--	local choiceButton3 = Font("data/fonts/DroidSans.ttf", 30, question_text_color)
+		--	local choiceButton4 = Font("data/fonts/DroidSans.ttf", 30, question_text_color)
 
 			-- Draw question
-			local question = self.current_question .. "."
+			local question = self.current_question .. ". "
 					.. self.mult_choice_quiz.questions[self.current_question].question
 			self.font:draw(self.question_area,
 				{x = 0, y = 0, height = self.question_area_height,
@@ -351,10 +341,10 @@ function MultipleChoiceView:render(surface)
 			choiceButton4:draw(surface,Rectangle(850,400,200,60):to_table(),"(4)." ..
 				self.mult_choice_quiz.questions[self.current_question].Choices[4])]]
 			-- Question buttons
-			local button_1_text =  "(1)." .. self.mult_choice_quiz.questions[self.current_question].Choices[1]
-			local button_2_text =  "(2)." .. self.mult_choice_quiz.questions[self.current_question].Choices[2]
-			local button_3_text =  "(3)." .. self.mult_choice_quiz.questions[self.current_question].Choices[3]
-			local button_4_text =  "(4)." .. self.mult_choice_quiz.questions[self.current_question].Choices[4]
+			local button_1_text =  "A. " .. self.mult_choice_quiz.questions[self.current_question].Choices[1]
+			local button_2_text =  "B. " .. self.mult_choice_quiz.questions[self.current_question].Choices[2]
+			local button_3_text =  "C. " .. self.mult_choice_quiz.questions[self.current_question].Choices[3]
+			local button_4_text =  "D. " .. self.mult_choice_quiz.questions[self.current_question].Choices[4]
 
 			self.question_button_1:set_textdata(button_1_text, Color(255,255,255,255),
 										{x = 0, y = 0}, 32,"data/fonts/DroidSans.ttf")
@@ -379,8 +369,8 @@ function MultipleChoiceView:render(surface)
 		elseif self.quiz_state == "DONE" then
 			-- Display the result from the whole quiz
 			self.mult_choice_quiz:calculate_score(self.correct_answer_number)
-			local quiz_result = "You answered " .. self.correct_answer_number ..
-								" questions correctly and " .. "\n" ..
+			local quiz_result = "You've answered " .. self.correct_answer_number ..
+								" questions correctly and " .. --"\n" ..
 								" your final score is " ..
 								self.mult_choice_quiz:get_score() .. "."
 			self.font:draw(self.question_area,
@@ -400,7 +390,6 @@ function MultipleChoiceView:render(surface)
 	gfx.update()
 	self:dirty(false)
 end
-
 
 
 return MultipleChoiceView
