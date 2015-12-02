f273791af5de5fce1999716b15f3b2dc4bf6a116
Robert Krogh <robkr627@student.liu.se>
2015-12-02 14:46:57 +0100

    Added base balance of 500 after creating new profile.

--- views/CreateProfileView2.lua
+++ views/CreateProfileView2.lua
@@ -165,7 +165,7 @@ function CreateProfileView2:load_view(button)
 			elseif self.content_pointer == 5 then
 				if self:control_input() then
 					self.profile = Profile(self.input_field.text, self.email, self.input_field2.text2, binary_button:get_value(), City.cities["london"])
-					self.profile:set_balance(0)
+					self.profile:set_balance(500)
 					self.profile:set_experience(0)
 					self.profile:set_password(self.password)
 					self.profile:set_id(0)
