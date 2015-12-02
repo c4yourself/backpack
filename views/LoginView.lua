5188c51bf9fc23599dbd9dbf607d6973e516db0b
Robert Krogh <robkr627@student.liu.se>
2015-12-02 14:14:45 +0100

    Changed to correct font color.

--- views/LoginView.lua
+++ views/LoginView.lua
@@ -38,10 +38,10 @@ function LoginView:__init(remote_control, profile_selection)
 	local color_disabled = Color(111, 222, 111, 255)
 	local button_size = {width = 100, height = 100}
 	self.button_cancel = button(button_color, color_selected, color_disabled, true, false, "views.ProfileSelection")
-	self.button_cancel_text = Font("data/fonts/DroidSans.ttf", 40, Color(0, 0, 0, 255))
+	self.button_cancel_text = Font("data/fonts/DroidSans.ttf", 40, Color(255, 255, 255, 255))
 	self.button_cancel_surface = SubSurface(screen, {width=500, height=100, x=700, y=530})
 	self.button_login = button(button_color, color_selected, color_disabled, true, false, "views.ProfileSelection")
-	self.button_login_text = Font("data/fonts/DroidSans.ttf", 40, Color(0, 0, 0, 255))
+	self.button_login_text = Font("data/fonts/DroidSans.ttf", 40, Color(255, 255, 255, 255))
 	self.button_login_surface = SubSurface(screen, {width=500, height=100, x=700, y=380})
 	-- buttons done
 
