fc42b88c419ed8a8d8b56459aaa8fda7206f7f65
Tobias Genborg (Developer) <tobias.genborg@gmail.com>
2015-12-07 09:58:18 +0100

    Refactored and commented View and ViewManager.

--- lib/view/View.lua
+++ lib/view/View.lua
@@ -27,6 +27,8 @@ function View:add_view(view, propagate_dirty)
 	end
 end
 
+--- Remove a view as a child view to this view
+-- @param view View that shall be removed
 function View:remove_view(view)
 	for i, v in ipairs(self.views) do
 		if v == view then
@@ -89,6 +91,7 @@ end
 
 --- Renders a view
 -- NOTE: Must be overloaded by a subclass
+-- @param surface {@Surface} or {@SubSurface} to render on
 function View:render(surface)
 	error("Render must be overloaded")
 end
