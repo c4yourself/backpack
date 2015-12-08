45cd20923c7eb9779120f79507a6596c8e237e85
Ellen Fransson (tester) <ellfr210@student.liu.se>
2015-12-08 14:12:54 +0100

    Changed attractions

--- lib/attractions.lua
+++ lib/attractions.lua
@@ -13,7 +13,7 @@ attractions.attraction = { paris = {
                                             "Tower in 2011. The tower is 324 meters tall and its base is 125 meters",
                                             "wide. The Eiffel Tower was the tallest man-made structure in the",
                                             "world for 41 years. Today it is the second-tallest structure in France.",
-                                            " The tower has three levels for visitors, with restaurants on the first",
+                                            "The tower has three levels for visitors, with restaurants on the first",
                                             "and second. The third level observatory's upper platform is 276 m",
                                             "above the ground. The climb from ground level to the first level is",
                                             "over 300 steps."}
@@ -67,7 +67,7 @@ attractions.attraction = { paris = {
                                       "worth visiting for its stunning views, fantastic nature and ",
                                       "city complex. It attracts quite a number of visitors during the",
                                       "Christmas holidays due to its famous and gigantic Christmas tree,",
-                                      " which is built over a floating platform that moves around the",
+                                      "which is built over a floating platform that moves around the",
                                       "lagoon."}
                                       },
                                       {name = "Tijuca National Park",
@@ -358,7 +358,7 @@ attractions.attraction = { paris = {
                                                    "the museum was broken into, and two mummies were reportedly",
                                                    "destroyed. Several artifacts were also shown to have been damaged.",
                                                    "Around 50 objects were lost. Since then 25 objects have been found,",
-                                                     "as of October 2015, it is open to the public."
+                                                   "as of October 2015, it is open to the public."
                                                    }
                                           },
                                           {name = "Al-Azhar mosque",
